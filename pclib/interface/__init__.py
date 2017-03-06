66e566e8c127e939efe35c47df7c70109cce2a8b
Shunning Jiang <sj634@cornell.edu>
2017-03-06 10:55:04 -0500

    Add subtractor. Having trouble with Reg

--- pclib/interface/__init__.py
+++ pclib/interface/__init__.py
@@ -2,4 +2,5 @@
 from TestSink    import TestSink
 from TestSource  import TestSource
 from registers   import RegEn, Reg
-from arithmetics import Mux
+from arithmetics import Mux, Subtractor
+from ports       import Port
--- pclib/interface/arithmetics.py
+++ pclib/interface/arithmetics.py
@@ -1,17 +1,34 @@
 from pymtl import *
+from ports import Port
 
 # N-input Mux
 
-class Mux(MethodComponent):
+class Mux(InterfaceComponent):
 
   def __init__( s, num_inputs = 2 ):
-    s.in_ = [0] * num_inputs
-    s.sel = 0
-    s.out = 0
+    s.in_ = [ Port(int) for _ in xrange(num_inputs) ]
+    s.sel = Port(int)
+    s.out = Port(int)
 
     @s.update
     def up_mux():
-      s.out = s.in_[ s.sel ]
+      s.out = s.in_[ s.sel.rd() ].rd()
 
   def line_trace( s ):
-    pass
+    return "[in%1d]" % (s.sel.rd() )
+
+class Subtractor(InterfaceComponent):
+
+  def __init__( s ):
+    s.in0 = Port(int)
+    s.in1 = Port(int)
+    s.out = Port(int)
+
+    @s.update
+    def up_subtract():
+      s.out.wr( s.in0.rd() - s.in1.rd() )
+
+  def line_trace( s ):
+    return "[%4s-%4s=%4s]" % (s.in0.line_trace(), \
+                              s.in1.line_trace(), \
+                              s.out.line_trace() )
