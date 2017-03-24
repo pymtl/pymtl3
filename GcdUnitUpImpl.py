8d15af9d8940acee1e5665c32b9f9edc793bcfd3
Shunning Jiang <sj634@cornell.edu>
2017-03-24 14:24:58 -0400

    add a gcd simulator

--- GcdUnitUpImpl.py
+++ GcdUnitUpImpl.py
@@ -184,17 +184,18 @@ class GcdUnit( Updates ):
       s.ctrl.is_b_zero  = s.dpath.is_b_zero
       s.ctrl.is_a_lt_b  = s.dpath.is_a_lt_b
 
-A = GcdUnit()
-A.elaborate()
-A.print_schedule()
+if __name__ == "__main__":
+  A = GcdUnit()
+  A.elaborate()
+  A.print_schedule()
 
-A.req_val = 1
-A.resp_rdy = 1
+  A.req_val = 1
+  A.resp_rdy = 1
 
-for cycle in xrange(10000000):
-  A.req_msg_a, A.req_msg_b = cycle+95827*(cycle&1), cycle+(19182)*(cycle&1)
-  # A.req_msg_a, A.req_msg_b = 60,35
-  A.cycle()
-  # print "req val:%d rdy:%d a:%6d b:%6d" % (A.req_val, A.req_rdy, A.req_msg_a, A.req_msg_b), \
-        # A.dpath.a_reg.line_trace(), A.dpath.b_reg.line_trace(), \
-        # "resp val:%d rdy:%d gcd:%6d" % (A.resp_val, A.resp_rdy, A.resp_msg )
+  for cycle in xrange(10000000):
+    A.req_msg_a, A.req_msg_b = cycle+95827*(cycle&1), cycle+(19182)*(cycle&1)
+    # A.req_msg_a, A.req_msg_b = 60,35
+    A.cycle()
+    # print "req val:%d rdy:%d a:%6d b:%6d" % (A.req_val, A.req_rdy, A.req_msg_a, A.req_msg_b), \
+          # A.dpath.a_reg.line_trace(), A.dpath.b_reg.line_trace(), \
+          # "resp val:%d rdy:%d gcd:%6d" % (A.resp_val, A.resp_rdy, A.resp_msg )
