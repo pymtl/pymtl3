10771f30df1f312bb78811602e27d457e5c3175a
Shunning Jiang <sj634@cornell.edu>
2017-03-25 20:26:21 -0400

    Finish up v3 Bits

--- pymtl/datatypes/bits_benchmark.py
+++ pymtl/datatypes/bits_benchmark.py
@@ -4,41 +4,65 @@ import timeit
 from Bits_v2 import Bits as Bv2
 from Bits_v3 import Bits as Bv3
 
-def test_lshift_arith_v2():
+def ubmark_arith_v2():
   g = Bv2(32, 0)
   for i in xrange(10000000):
     a = Bv2(32, 12938676)
     b = Bv2(32, i & 127)
-    c = a + b
-    d = a << b
+    c = a >> b
+    d = a + b
     c = d | (b ^ c)
-    d = c << (b & Bv2(31, 7) )
+    d = c + (b & Bv2(31, 7) )
     g += d
 
   return g
 
-def test_lshift_arith_v3():
+def ubmark_arith_v3():
   g = Bv3(32, 0)
   for i in xrange(10000000):
     a = Bv3(32, 12938676)
     b = Bv3(32, i & 127)
-    c = a + b
-    d = a << b
+    c = a >> b
+    d = a + b
     c = d | (b ^ c)
-    d = c << (b & Bv3(31, 7) )
+    d = c + (b & Bv3(31, 7) )
     g += d
 
   return g
 
-# print "[lshift_arith] v3 avg time:",sum(timeit.repeat( test_lshift_arith_v3, repeat=5, number=10 ))/5
-# print "[lshift_arith] v3", test_lshift_arith_v3()
-# print
+print "[arith] v3 avg time:",sum(timeit.repeat( ubmark_arith_v3, repeat=5, number=10 ))/5
+print "[arith] v3", ubmark_arith_v3()
 
-# print "[lshift_arith] v2 avg time:",sum(timeit.repeat( test_lshift_arith_v2, repeat=5, number=10 ))/5
-# print "[lshift_arith] v2", test_lshift_arith_v2()
-# print
+print "[arith] v2 avg time:",sum(timeit.repeat( ubmark_arith_v2, repeat=5, number=10 ))/5
+print "[arith] v2", ubmark_arith_v2()
+print
 
-def test_idx_v2():
+def ubmark_lshift_v2():
+  g = Bv2(32, 0)
+  for i in xrange(10000000):
+    a = Bv2(32, 12938676)
+    b = Bv2(32, i & 255)
+    g += (a << b)
+
+  return g
+
+def ubmark_lshift_v3():
+  g = Bv3(32, 0)
+  for i in xrange(10000000):
+    a = Bv3(32, 12938676)
+    b = Bv3(32, i & 255)
+    g += (a << b)
+
+  return g
+
+print "[lshift_arith] v3 avg time:",sum(timeit.repeat( ubmark_lshift-v3, repeat=5, number=10 ))/5
+print "[lshift_arith] v3", ubmark_lshift_v3()
+
+print "[lshift_arith] v2 avg time:",sum(timeit.repeat( ubmark_lshift_v2, repeat=5, number=10 ))/5
+print "[lshift_arith] v2", ubmark_lshift_v2()
+print
+
+def ubmark_idx_v2():
   g = Bv2(32, 0)
   for i in xrange(10000000):
     a = Bv2(32, i)
@@ -48,7 +72,7 @@ def test_idx_v2():
 
   return g
 
-def test_idx_v3():
+def ubmark_idx_v3():
   g = Bv3(32, 0)
   for i in xrange(10000000):
     a = Bv3(32, i)
@@ -58,38 +82,38 @@ def test_idx_v3():
 
   return g
 
-print "[idx] v3 avg time:",sum(timeit.repeat( test_idx_v3, repeat=5, number=10 ))/5
-print "[idx] v3", test_idx_v3()
+print "[idx] v3 avg time:",sum(timeit.repeat( ubmark_idx_v3, repeat=5, number=10 ))/5
+print "[idx] v3", ubmark_idx_v3()
+
+print "[idx] v2 avg time:",sum(timeit.repeat( ubmark_idx_v2, repeat=5, number=10 ))/5
+print "[idx] v2", ubmark_idx_v2()
 print
 
-print "[idx] v2 avg time:",sum(timeit.repeat( test_idx_v2, repeat=5, number=10 ))/5
-print "[idx] v2", test_idx_v2()
-print
-
-def test_slice_v2():
+def ubmark_slice_v2():
   g = Bv2(32, 0)
   for i in xrange(10000000):
     a = Bv2(32, i)
-    b = Bv2(32, i & 31)
-    a[int(b)] = a[b] ^ 1 # pymtl v2 bits bug
-    g += a[b]
+    b = Bv2(6, i & 15)
+    c = Bv2(6, 32 - (i & 7) )
+    a[int(b):int(c)] = a[int(b):int(c)] ^ 2147483647
+    g += a[int(b):int(c)]
 
   return g
 
-def test_slice_v3():
+def ubmark_slice_v3():
   g = Bv3(32, 0)
   for i in xrange(10000000):
     a = Bv3(32, i)
-    b = Bv3(32, i & 31)
-    a[b] = a[b] ^ 1
-    g += a[b]
+    b = Bv3(6, i & 15)
+    c = Bv3(6, 32 - (i & 7))
+    a[b:c] = a[b:c] ^ 2147483647
+    g += a[b:c]
 
   return g
 
-print "[slice] v3 avg time:",sum(timeit.repeat( test_slice_v3, repeat=5, number=10 ))/5
-print "[slice] v3", test_slice_v3()
-print
+print "[slice] v3 avg time:",sum(timeit.repeat( ubmark_slice_v3, repeat=5, number=10 ))/5
+print "[slice] v3", ubmark_slice_v3()
 
-print "[slice] v2 avg time:",sum(timeit.repeat( test_slice_v2, repeat=5, number=10 ))/5
-print "[slice] v2", test_slice_v2()
+print "[slice] v2 avg time:",sum(timeit.repeat( ubmark_slice_v2, repeat=5, number=10 ))/5
+print "[slice] v2", ubmark_slice_v2()
 print
