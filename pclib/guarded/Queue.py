73415b98b4121f8d221dcbbab0fdc87ac9c0aba5
Shunning Jiang <sj634@cornell.edu>
2017-04-14 11:46:08 -0400

    [pymtl/method] guarded method support

---
+++ pclib/guarded/Queue.py
@@ -0,0 +1,40 @@
+from collections import deque
+from pymtl import *
+
+class PipeQueue( MethodsGuard ):
+  def __init__( s, size=1 ):
+    s.queue = deque(maxlen=size)
+    s.add_constraints(
+      M(s.deq) < M(s.enq), # pipe behavior
+    )
+
+  @guard_rdy(lambda s: len(s.queue) < s.queue.maxlen)
+  def enq( s, v ):
+    s.queue.appendleft(v)
+
+  @guard_rdy(lambda s: len(s.queue) > 0)
+  def deq( s ):
+    return s.queue.pop()
+
+  def line_trace( s ):
+    return "Q{:10}".format("".join( [ "[%d]"%x for x in s.queue ]) )
+
+class BypassQueue( MethodsGuard ):
+  def __init__( s, size=1 ):
+    s.queue = deque(maxlen=size)
+    s.add_constraints(
+      M(s.enq) < M(s.deq), # bypass behavior
+    )
+
+  @guard_rdy(lambda s: len(s.queue) < s.queue.maxlen)
+  def enq( s, v ):
+    s.queue.appendleft(v)
+
+  @guard_rdy(lambda s: len(s.queue) > 0)
+  def deq( s ):
+    return s.queue.pop()
+
+  def line_trace( s ):
+    return "Q{:10}".format("".join( [ "[%d]"%x for x in s.queue ]) )
+  def line_trace( s ):
+    return "Q{:10}".format("".join( [ "[%d]"%x for x in s.queue ]) )
