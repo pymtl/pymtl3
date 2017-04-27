9f15a77c680b398122b3f021101f3acdd7a60bdc
Shunning Jiang <sj634@cornell.edu>
2017-04-27 10:14:41 -0400

    [pclib] EnRdyBundle and RTL Queues

--- pclib/ifcs/__init__.py
+++ pclib/ifcs/__init__.py
@@ -1,4 +1,5 @@
 
 from ValRdyBundle import ValRdyBundle
+from EnRdyBundle  import EnRdyBundle
 from valrdy       import valrdy_to_str
 from MemMsg       import MemReqMsg, MemRespMsg
--- pclib/update/__init__.py
+++ pclib/update/__init__.py
@@ -4,3 +4,4 @@ from TestSource  import TestSource, TestSourceValRdy, StreamSource
 from registers   import Reg, RegEn
 from arithmetics import Mux, LShifter, RShifter, Adder, Subtractor
 from arithmetics import ZeroComp, LTComp, LEComp
+from queues      import PipeQueue1RTL, BypassQueue1RTL, NormalQueue1RTL
