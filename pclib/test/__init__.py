a680acb73f2133672879bbbff2a1aa0324b50a62
Shunning Jiang <sj634@cornell.edu>
2017-04-28 23:46:34 -0400

    CL src and sink

--- pclib/test/__init__.py
+++ pclib/test/__init__.py
@@ -1,4 +1,4 @@
 
-from TestSource import TestSource, StreamSource, TestSourceEnRdy
-from TestSink   import TestSink,   StreamSink,   TestSinkEnRdy
+from TestSource import TestSource, StreamSource, TestSourceEnRdy, TestSourceCL
+from TestSink   import TestSink,   StreamSink,   TestSinkEnRdy,   TestSinkCL
 from test_utils import mk_test_case_table
