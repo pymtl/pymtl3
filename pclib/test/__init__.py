945450f1255cc69181d00049393c2d5e71dd6a47
Shunning Jiang <sj634@cornell.edu>
2017-05-09 18:54:02 -0400

    fl-fl memfillunit

--- pclib/test/__init__.py
+++ pclib/test/__init__.py
@@ -2,4 +2,4 @@
 from TestSource import TestSourceValRdy, StreamSourceValRdy, TestSourceEnRdy, TestSourceCL, TestSource
 from TestSink   import TestSinkValRdy,   StreamSinkValRdy,   TestSinkEnRdy,   TestSinkCL,   TestSink
 from test_utils import mk_test_case_table
-from TestMemory import TestMemoryCL
+from TestMemory import TestMemoryFL, TestMemoryCL
