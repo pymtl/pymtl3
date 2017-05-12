968b8ad9a6508eb94d1236c8b5114b671e48ab7e
Shunning Jiang <sj634@cornell.edu>
2017-05-12 15:14:55 -0400

    Bits impl improvement

--- pymtl/datatypes/Bits.py
+++ pymtl/datatypes/Bits.py
@@ -10,93 +10,98 @@ class Bits( object ):
 
   # Arithmetics
   def __getitem__( self, idx ):
+    sv = int(self.value)
+
     if isinstance( idx, slice ):
       start, stop = int(idx.start), int(idx.stop)
       assert not idx.step and start < stop
-      return Bits( stop-start, (self.value & ((1 << stop) - 1)) >> start )
+      return Bits( stop-start, (sv & ((1 << stop) - 1)) >> start )
 
     i = int(idx)
     assert 0 <= i < self.nbits
-    return Bits( 1, (self.value >> i) & 1 )
+    return Bits( 1, (sv >> i) & 1 )
 
   def __setitem__( self, idx, v ):
+    sv = int(self.value)
+
     if isinstance( idx, slice ):
       start, stop = int(idx.start), int(idx.stop)
       assert not idx.step and start < stop
 
-      self.value = (int(self.value) & (~((1 << stop) - (1 << start)))) | \
-                                      ((int(v) & ((1 << (stop - start)) - 1)) << start)
+      self.value = (sv & (~((1 << stop) - (1 << start)))) | \
+                   ((int(v) & ((1 << (stop - start)) - 1)) << start)
       return
 
     i = int(idx)
     assert 0 <= i < self.nbits
-    self.value = (int(self.value) & ~(1 << i)) | ((int(v) & 1) << i)
+    self.value = (sv & ~(1 << i)) | ((int(v) & 1) << i)
 
   def __add__( self, other ):
-    return Bits( self.nbits, self.value + int(other) )
+    return Bits( self.nbits, int(self.value) + int(other) )
 
   def __radd__( self, other ):
     return self.__add__( other )
 
   def __sub__( self, other ):
-    return Bits( self.nbits, self.value - int(other) )
+    return Bits( self.nbits, int(self.value) - int(other) )
 
   def __mul__( self, other ):
-    return Bits( self.nbits, self.value * int(other) )
+    return Bits( self.nbits, int(self.value) * int(other) )
 
   def __and__( self, other ):
-    return Bits( self.nbits, self.value & int(other) )
+    return Bits( self.nbits, int(self.value) & int(other) )
 
   def __rand__( self, other ):
     return self.__and__( other )
 
   def __or__( self, other ):
-    return Bits( self.nbits, self.value | int(other) )
+    return Bits( self.nbits, int(self.value) | int(other) )
 
   def __ror__( self, other ):
     return self.__or__( other )
 
   def __xor__( self, other ):
-    return Bits( self.nbits, self.value ^ int(other) )
+    return Bits( self.nbits, int(self.value) ^ int(other) )
 
   def __rxor__( self, other ):
     return self.__xor__( other )
 
   def __invert__( self ):
-    return Bits( self.nbits, ~self.value )
+    return Bits( self.nbits, ~int(self.value) )
 
   def __lshift__( self, other ):
+    nb = int(self.nbits)
     # TODO this doesn't work perfectly. We need a really smart
     # optimization that avoids the guard totally
-    if int(other) >= self.nbits: return Bits( self.nbits )
-    return Bits( self.nbits, self.value << int(other) )
+    if int(other) >= nb: return Bits( nb )
+    return Bits( nb, int(self.value) << int(other) )
 
   def __rshift__( self, other ):
-    return Bits( self.nbits, self.value >> int(other) )
+    return Bits( self.nbits, int(self.value) >> int(other) )
 
   def __eq__( self, other ):
-    return Bits( 1, self.value == int(other) )
+    return Bits( 1, int(self.value) == int(other) )
 
   def __ne__( self, other ):
-    return Bits( 1, self.value != int(other) )
+    return Bits( 1, int(self.value) != int(other) )
 
   def __lt__( self, other ):
-    return Bits( 1, self.value < int(other) )
+    return Bits( 1, int(self.value) < int(other) )
 
   def __le__( self, other ):
-    return Bits( 1, self.value <= int(other) )
+    return Bits( 1, int(self.value) <= int(other) )
 
   def __gt__( self, other ):
-    return Bits( 1, self.value > int(other) )
+    return Bits( 1, int(self.value) > int(other) )
 
   def __ge__( self, other ):
-    return Bits( 1, self.value >= int(other) )
+    return Bits( 1, int(self.value) >= int(other) )
 
   def __nonzero__( self ):
     return int(self.value) != 0
 
   def __int__( self ):
-    return self.value
+    return int(self.value)
 
   def int( self ):
     if self.value >> (self.nbits - 1):
@@ -104,11 +109,10 @@ class Bits( object ):
     return int(self.value)
 
   def uint( self ):
-    assert self.value >= 0
     return int(self.value)
 
   def __index__( self ):
-    return self.value
+    return int(self.value)
 
   # Print
 
@@ -116,7 +120,7 @@ class Bits( object ):
     return "Bits{}( {} )".format( self.nbits, self.hex() )
 
   def __str__(self):
-    str = "{:x}".format(self.value).zfill(((self.nbits-1)>>2)+1)
+    str = "{:x}".format(int(self.value)).zfill(((self.nbits-1)>>2)+1)
     return str
 
   def __oct__( self ):
@@ -128,15 +132,15 @@ class Bits( object ):
     return self.hex()
 
   def bin(self):
-    str = "{:b}".format(self.value).zfill(self.nbits)
+    str = "{:b}".format(int(self.value)).zfill(self.nbits)
     return "0b"+str
 
   def oct( self ):
-    str = "{:o}".format(self.value).zfill(((self.nbits-1)>>1)+1)
+    str = "{:o}".format(int(self.value)).zfill(((self.nbits-1)>>1)+1)
     return "0o"+str
 
   def hex( self ):
-    str = "{:x}".format(self.value).zfill(((self.nbits-1)>>2)+1)
+    str = "{:x}".format(int(self.value)).zfill(((self.nbits-1)>>2)+1)
     return "0x"+str
 
 bits_template = """
