741bdaae1c4ce11b05b8b94992ccb6178497afb2
Shunning Jiang <sj634@cornell.edu>
2017-05-14 19:46:30 -0400

    mem fill finished

--- MemFillUnit.py
+++ MemFillUnit.py
@@ -1,26 +1,29 @@
 from pymtl import *
-from pclib.ifcs import MemIfcCL, MemIfcFL, MemReqMsg, MemRespMsg
+from pclib.ifcs import MemIfcRTL, MemIfcCL, MemIfcFL, MemReqMsg, MemRespMsg
 from pclib.test import TestMemoryFL, TestMemoryCL
+from pclib.update import Reg
 
 class MemFillFL( MethodsAdapt ):
   def __init__( s, base_addr=0x1000, size=100 ):
-    s.memifc = MemIfcFL()
+    s.mem = MemIfcFL()
 
     s.finished = False
 
     @s.pausable_update
     def up_fill():
 
-      # Stream the writes
+      if not s.finished:
 
-      for i in xrange(size):
-        s.memifc.write( base_addr+((i&131071)<<2), 4, i&131071)
+        # Stream the writes
 
-      # Check the answers
+        for i in xrange(size):
+          s.mem.write( base_addr+i*4, 4, i )
 
-      for i in xrange(size):
-        assert s.memifc.read( base_addr+((i&131071)<<2), 4 ) == i&131071
-        print "#{} passed".format( i )
+        # Check the answers
+
+        for i in xrange(size):
+          assert s.mem.read( base_addr+i*4, 4 ) == i
+          print "#{} passed".format( i )
 
       s.finished = True
 
@@ -33,20 +36,20 @@ class MemFillFL( MethodsAdapt ):
 class MemFillCL( MethodsAdapt ):
 
   def __init__( s, base_addr=0x1000, size=100 ):
-    s.req_type  = MemReqMsg(32,32,32) # 32 bit opaque
-    s.resp_type = MemRespMsg(32,32)   # 32 bit opaque
+    s.ReqType  = MemReqMsg(8,32,32)
+    s.RespType = MemRespMsg(8,32)
 
-    s.memifc = MemIfcCL( (s.req_type, s.resp_type) )
-    s.memifc.resp.enq |= s.recv
-    s.memifc.resp.rdy |= s.recv_rdy
+    s.mem = MemIfcCL( (s.ReqType, s.RespType) )
+    s.mem.resp.enq |= s.recv
+    s.mem.resp.rdy |= s.recv_rdy
 
     s.i    = 0
     s.size = size
 
     @s.update
     def up_fill():
-      if s.memifc.req.rdy() and s.i < s.size:
-        s.memifc.req.enq( s.req_type.mk_wr( s.i+100, base_addr+(s.i<<2), 4, s.i ) )
+      if s.mem.req.rdy() and s.i < s.size:
+        s.mem.req.enq( s.ReqType.mk_wr( s.i+100, base_addr+s.i*4, 4, s.i ) )
         s.i += 1
 
     s.last_recv = -1
@@ -63,6 +66,66 @@ class MemFillCL( MethodsAdapt ):
   def line_trace( s ):
     return "Last req send: #%d | Last resp recv: #%d" % (s.i, s.last_recv)
 
+class MemFillRTL( MethodsAdapt ):
+
+  def __init__( s, base_addr=0x1000, size=100 ):
+    s.ReqType  = MemReqMsg(8,32,32)
+    s.RespType = MemRespMsg(8,32)
+
+    s.mem = MemIfcRTL( (s.ReqType, s.RespType) )
+
+    s.state = Reg( Bits2 )
+
+    s.STATE_IDLE = 0
+    s.STATE_CALC = 1
+
+    s.i    = Reg( Bits32 )
+    s.size = Reg( Bits32 )
+    s.fini = Reg( Bits1 )
+    s.ts = 0
+
+    @s.update
+    def up_send():
+      
+      curr_state  = s.state.out
+
+      s.state.in_ = s.state.out
+      s.i.in_     = s.i.out
+      s.size.in_  = s.size.out
+      s.mem.resp.rdy = Bits1( 0 )
+
+      if curr_state == s.STATE_IDLE:
+        s.mem.req.en   = Bits1( 0 )
+        s.mem.req.msg  = s.ReqType()
+
+        s.i.in_    = Bits32( 0    )
+        s.size.in_ = Bits32( size )
+
+        if not s.fini.out:
+          s.state.in_ = s.STATE_CALC
+
+      elif curr_state == s.STATE_CALC:
+        s.mem.resp.rdy = Bits1( 1 )
+        s.mem.req.en   = Bits1( 0 )
+        s.mem.req.msg  = s.ReqType()
+
+        if s.mem.req.rdy:# and s.ts & 1:
+          s.mem.req.en  = Bits1( 1 )
+          s.mem.req.msg = s.ReqType.mk_wr( s.i.out+100, base_addr+s.i.out*4, 4, s.i.out )
+          s.i.in_ = s.i.out + 1
+
+        s.ts += 1
+
+        if s.i.in_ == s.size.out:
+          s.state.in_ = s.STATE_IDLE
+          s.fini.in_  = Bits1( 1 )
+
+  def done( s ):
+    return s.fini.out
+
+  def line_trace( s ):
+    return "{}/{}".format( s.i.line_trace(), s.size.out )
+
 class Harness( MethodsAdapt ):
   def __init__( s, level='fl', base_addr=0x1000, size=100, mem_level='fl' ):
 
@@ -70,23 +133,23 @@ class Harness( MethodsAdapt ):
       s.mem = TestMemoryFL()
 
       if   level == 'fl' :
-        s.fill = MemFillFL( base_addr, size )( memifc = s.mem.ifc )
+        s.fill = MemFillFL( base_addr, size )( mem = s.mem.ifc )
       else:
         if level == 'cl' :  s.fill = MemFillCL ( base_addr, size )
         else:               s.fill = MemFillRTL( base_addr, size )
 
-        s.connect( s.fill.memifc, s.mem.ifc )
+        s.connect( s.fill.mem, s.mem.ifc )
 
     if mem_level == 'cl':
-      s.mem = TestMemoryCL( 1, [ MemReqMsg(32,32,32) ], [ MemRespMsg(32,32) ] )
+      s.mem = TestMemoryCL( 1, [ MemReqMsg(8,32,32) ], [ MemRespMsg(8,32) ] )
 
       if   level == 'cl' :
-        s.fill = MemFillCL( base_addr, size )( memifc = s.mem.ifcs[0] )
+        s.fill = MemFillCL( base_addr, size )( mem = s.mem.ifcs[0] )
       else:
         if level == 'fl' :  s.fill = MemFillFL ( base_addr, size )
         else:               s.fill = MemFillRTL( base_addr, size )
 
-        s.connect( s.fill.memifc, s.mem.ifcs[0] )
+        s.connect( s.fill.mem, s.mem.ifcs[0] )
 
   def done( s ):
     return s.fill.done()
@@ -96,9 +159,11 @@ class Harness( MethodsAdapt ):
 
 if __name__ == "__main__":
 
-  for mem_level in [ "cl" ]: 
-    for fill_level in [ "fl" ]: # [ "fl", "cl", "rtl"]:
-      A = Harness( fill_level, 0x100, 10, mem_level )
+  for mem_level in [ "fl","cl" ]: 
+    for fill_level in [ "fl", "cl", "rtl"]:
+      print
+      print "----- fill_{} + mem_{} -----".format(fill_level, mem_level)
+      A = Harness( fill_level, 0x1000, 10, mem_level )
       A.elaborate()
       A.print_schedule()
 
--- pclib/ifcs/MemIfcAdapters.py
+++ pclib/ifcs/MemIfcAdapters.py
@@ -10,9 +10,23 @@ class MemIfcAdapter( MethodsAdapt ):
     if Type1: s.req_type1, s.resp_type1 = Type1[0], Type1[1]
     if Type2: s.req_type2, s.resp_type2 = Type2[0], Type2[1]
 
+    assert level1 != level2, "This is when we need adapters"
     # only support these right now
 
-    if   level1 == 'cl' and level2 == 'fl': # needs adapter
+    if   level1 == 'fl' and level2 == 'cl':
+
+      s.msg = None
+
+      s.left  = MemIfcFL()
+      s.left.read  |= s.read_
+      s.left.write |= s.write_
+      s.left.amo   |= s.amo_
+
+      s.right = MemIfcCL( Type2 )
+      s.right.resp.enq |= s.recv_
+      s.right.resp.rdy |= s.recv_rdy_
+
+    elif level1 == 'cl' and level2 == 'fl': # needs adapter
 
       s.left  = MemIfcCL( Type1 )
       s.left.req.enq |= s.recv_
@@ -20,10 +34,11 @@ class MemIfcAdapter( MethodsAdapt ):
 
       s.right = MemIfcFL()
 
-      s.msg = None
+      s.msg  = None
+      s.resp = None
 
       @s.update
-      def up_memifc_fl_cl_adapter():
+      def up_memifc_cl_fl_blk():
 
         if s.left.resp.rdy() and s.msg:
           req = s.msg
@@ -45,33 +60,56 @@ class MemIfcAdapter( MethodsAdapt ):
           s.msg = None
 
       s.add_constraints(
-        U(up_memifc_fl_cl_adapter) < M(s.left.req.enq), # bypass behavior, send < recv
-        U(up_memifc_fl_cl_adapter) < M(s.left.req.rdy),
+        U(up_memifc_cl_fl_blk) < M(s.left.req.enq), # pipe behavior, send < recv
+        U(up_memifc_cl_fl_blk) < M(s.left.req.rdy),
       )
 
-    elif level1 == 'fl' and level2 == 'cl':
+    elif level1 == 'fl' and level2 == 'rtl':
+      pass
 
-      s.msg = None
+    elif level1 == 'rtl' and level2 == 'fl':
 
-      s.left  = MemIfcFL()
-      s.left.read  |= s.read_
-      s.left.write |= s.write_
-      s.left.amo   |= s.amo_
+      s.left  = MemIfcRTL( Type1 )
+      s.right = MemIfcFL()
 
-      s.right = MemIfcCL( Type2 )
-      s.right.resp.enq |= s.recv_
-      s.right.resp.rdy |= s.recv_rdy_
+      @s.update
+      def up_memifc_req_rdy_rtl_fl():
+        s.left.req.rdy = Bits1( 1 )
 
-    else:
-      if   level1 == 'fl':  s.left  = MemIfcFL ()
-      elif level1 == 'cl':  s.left  = MemIfcCL ( Type1 )
-      else:                 s.left  = MemIfcRTL( Type1 )
+      @s.update
+      def up_memifc_req_en_rtl_fl():
+        if s.left.req.en & s.left.resp.rdy:
+          req = s.left.req.msg
 
-      if   level1 == 'fl':  s.right = MemIfcFL ()
-      elif level1 == 'cl':  s.right = MemIfcCL ( Type2 )
-      else:                 s.right = MemIfcRTL( Type2 )
+          len = req.len if req.len else ( s.req_type1.data.nbits >> 3 )
 
-      s.connect( s.left, s.right )
+          if   req.type_ == s.req_type1.TYPE_READ:
+            resp = s.resp_type1.mk_rd( req.opaque, len, s.right.read( req.addr, len ) )
+
+          elif req.type_ == s.req_type1.TYPE_WRITE:
+            s.right.write( req.addr, len, req.data )
+            resp = s.resp_type1.mk_wr( req.opaque )
+
+          else: # AMOS
+            resp = s.resp_type1.mk_msg( req.type_, req.opaque, 0, len, \
+                                 s.right.amo( req.type_, req.addr, len, req.data ) )
+
+          s.left.resp.en  = Bits1( 1 )
+          s.left.resp.msg = resp
+
+    elif level1 == 'cl' and level2 == 'rtl':
+      s.left  = MemIfcCL ( Type1 )
+      s.right = MemIfcRTL( Type2 )
+
+      s.connect( s.left.req, s.right.req )
+      s.connect( s.right.resp, s.left.resp )
+
+    elif level1 == 'rtl' and level2 == 'cl':
+      s.left  = MemIfcRTL( Type1 )
+      s.right = MemIfcCL ( Type2 )
+
+      s.connect( s.left.req, s.right.req )
+      s.connect( s.right.resp, s.left.resp )
 
   def recv_( s, msg ): # Recv can be used for left's req, or right's resp
     s.msg = msg
@@ -113,15 +151,16 @@ class MemIfcAdapter( MethodsAdapt ):
     while not s.right.req.rdy():
       greenlet.getcurrent().parent.switch(0)
 
+    s.right.req.enq( s.req_type2.mk_msg( amo, 0, addr, nbytes ) )
+
     while not s.msg:
       greenlet.getcurrent().parent.switch(0)
-
-    s.right.req.enq( s.req_type2.mk_msg( amo, 0, addr, nbytes ) )
 
     ret = s.msg.data
     s.msg = None
     return ret
 
-for l1 in [ 'rtl', 'cl', 'fl' ]:
-  for l2 in [ 'rtl', 'cl', 'fl' ]:
-    register_adapter( MemIfcAdapter, l1, l2 )
+for l1 in [ 'fl','cl','rtl' ]:
+  for l2 in [ 'fl','cl','rtl' ]:
+    if l1 != l2:
+      register_adapter( MemIfcAdapter, l1, l2 )
