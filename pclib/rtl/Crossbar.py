cc88205819f021fdba0eebf2391c74c5f28d017d
Berkin Ilbeyi <berkin@csl.cornell.edu>
2017-11-17 15:50:13 -0500

    [pclib.rtl] Add crossbar from v2

---
+++ pclib/rtl/Crossbar.py
@@ -0,0 +1,27 @@
+#=======================================================================
+# Crossbar.py
+#=======================================================================
+
+from pymtl import *
+
+class Crossbar( RTLComponent ):
+
+  def __init__( s, nports, dtype ):
+
+    sel_nbits = clog2( nports )
+
+    s.in_ = [ InVPort  ( dtype )     for _ in range( nports ) ]
+    s.out = [ OutVPort ( dtype )     for _ in range( nports ) ]
+    s.sel = [ InVPort  ( mk_bits( sel_nbits ) ) for _ in range( nports ) ]
+
+    @s.update
+    def comb_logic():
+
+      for i in range( nports ):
+        s.out[i] = s.in_[ s.sel[ i ] ]
+
+  def line_trace( s ):
+    in_str  = ' '.join( [ str(x) for x in s.in_ ] )
+    sel_str = ' '.join( [ str(x) for x in s.sel ] )
+    out_str = ' '.join( [ str(x) for x in s.out ] )
+    return '{} ( {} ) {}'.format( in_str, sel_str, out_str )
---
+++ pclib/rtl/Crossbar_test.py
@@ -0,0 +1,80 @@
+#=======================================================================
+# Crossbar_test.py
+#=======================================================================
+
+from pymtl      import *
+from Crossbar   import Crossbar
+
+class TestVectorSimulator( object ):
+
+  def __init__( self, model, test_vectors,
+                set_inputs_func, verify_outputs_func, wait_cycles = 0 ):
+
+    self.model               = model
+    self.set_inputs_func     = set_inputs_func
+    self.verify_outputs_func = verify_outputs_func
+    self.test_vectors        = test_vectors
+    self.wait_cycles         = wait_cycles
+
+  def run_test( self ):
+
+    SimRTLPass().apply( self.model )
+
+    print()
+    for test_vector in self.test_vectors:
+
+      # Set inputs
+      self.set_inputs_func( self.model, test_vector )
+      self.model.tick()
+
+      # Print the line trace
+      print self.model.line_trace()
+
+      # Verify outputs
+      self.verify_outputs_func( self.model, test_vector )
+
+#-----------------------------------------------------------------------
+# run_test_crossbar
+#-----------------------------------------------------------------------
+def run_test_crossbar( model, test_vectors ):
+
+  # Instantiate and elaborate the model
+
+  model.elaborate()
+
+  # Define functions mapping the test vector to ports in model
+
+  num_inputs = len( model.in_ )
+
+  def tv_in( model, test_vector ):
+    n = num_inputs
+    for i in range(num_inputs):
+      model.in_[i] = test_vector[i]
+      model.sel[i] = test_vector[n+i]
+
+  def tv_out( model, test_vector ):
+    n = 2*num_inputs
+    for i in range(num_inputs):
+      assert model.out[i] == test_vector[n+i]
+
+  # Run the test
+
+  sim = TestVectorSimulator( model, test_vectors, tv_in, tv_out )
+  sim.run_test()
+
+#-----------------------------------------------------------------------
+# test_crossbar3
+#-----------------------------------------------------------------------
+def test_crossbar3( dump_vcd, test_verilog ):
+  model = Crossbar( 3, Bits16 )
+  model.vcd_file = dump_vcd
+  if test_verilog:
+    model = TranslationTool( model )
+  run_test_crossbar( model, [
+    [ 0xdead, 0xbeef, 0xcafe, 0, 1, 2, 0xdead, 0xbeef, 0xcafe ],
+    [ 0xdead, 0xbeef, 0xcafe, 0, 2, 1, 0xdead, 0xcafe, 0xbeef ],
+    [ 0xdead, 0xbeef, 0xcafe, 1, 2, 0, 0xbeef, 0xcafe, 0xdead ],
+    [ 0xdead, 0xbeef, 0xcafe, 1, 0, 2, 0xbeef, 0xdead, 0xcafe ],
+    [ 0xdead, 0xbeef, 0xcafe, 2, 1, 0, 0xcafe, 0xbeef, 0xdead ],
+    [ 0xdead, 0xbeef, 0xcafe, 2, 0, 1, 0xcafe, 0xdead, 0xbeef ],
+  ])
