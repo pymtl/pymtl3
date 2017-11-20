9511af80e8c6e466c8dff2650756c6027de2cb67
Berkin Ilbeyi <berkin@csl.cornell.edu>
2017-11-20 11:08:35 -0500

    [pclib.rtl] Add new components to __init__

--- pclib/rtl/__init__.py
+++ pclib/rtl/__init__.py
@@ -1,10 +1,14 @@
 
-from TestSink    import TestBasicSink,   TestSinkValRdy,   TestSink
+from TestSink    import ( TestBasicSink, TestSinkValRdy, TestSink,
+                          TestSinkUnorderedValRdy )
 from TestSource  import TestBasicSource, TestSourceValRdy, TestSource
 from registers   import Reg, RegEn, RegRst, RegEnRst
 from RegisterFile import RegisterFile
 from arithmetics import Mux, LShifter, RShifter, Incrementer, Adder, Subtractor
 from arithmetics import ZeroComp, LTComp, LEComp
+from arbiters    import RoundRobinArbiter, RoundRobinArbiterEn
+from Crossbar    import Crossbar
 # from queues      import PipeQueue1RTL, BypassQueue1RTL, NormalQueue1RTL
-from valrdy_queues import PipeQueue1RTL, BypassQueue1RTL, NormalQueue1RTL
+from valrdy_queues import ( PipeQueue1RTL, BypassQueue1RTL, NormalQueue1RTL,
+                            NormalQueueRTL )
 from TestMemory  import TestMemoryRTL
