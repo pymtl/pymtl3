5adb75b06fc3902cf4059febc04d81d9f09c0413
Shunning Jiang <sj634@cornell.edu>
2019-01-15 19:50:36 -0500

    interface(portbundle) work out of box

--- pymtl/dsl/test/ComponentLevel5_test.py
+++ pymtl/dsl/test/ComponentLevel5_test.py
@@ -180,3 +180,85 @@ def test_simple_src_up_sink():
 
 
   _test_model( Top )
+
+def test_method_interface():
+
+  class RecvIfcCL( Interface ):
+    def construct( s, recv=None, rdy=None):
+      s.msg = CalleePort( recv )
+      s.rdy = CalleePort( rdy )
+
+  class SendIfcCL( Interface ):
+    def construct( s ):
+      s.msg = CallerPort()
+      s.rdy = CallerPort()
+
+  class SimpleTestSourceIfc( ComponentLevel5 ):
+
+    def construct( s, msgs ):
+      s.msgs = deque( msgs )
+
+      s.req = SendIfcCL()
+
+      s.v = 0
+      @s.update
+      def up_src():
+        s.v = None
+        if s.req.rdy() and s.msgs:
+          s.v = s.msgs.popleft()
+          s.req.msg( s.v )
+
+    def done( s ):
+      return not s.msgs
+
+    def line_trace( s ):
+      return "{:4}".format( "" if s.v is None else s.v )
+
+  class SimpleTestSinkIfc( ComponentLevel5 ):
+
+    def resp_( s, msg ):
+      ref = s.msgs[ s.idx ]
+      s.idx += 1
+
+      if msg != ref:
+        raise TestSinkError( """
+  The test sink received an incorrect message!
+  - sink name    : {}
+  - msg number   : {}
+  - expected msg : {}
+  - actual msg   : {}
+  """.format( s, s.idx, ref, msg )
+        )
+
+    def resp_rdy_( s ):
+      return True
+
+    def construct( s, msgs ):
+      s.msgs = list( msgs )
+      s.idx  = 0
+
+      s.resp = RecvIfcCL( recv = s.resp_, rdy = s.resp_rdy_ )
+
+    def done( s ):
+      return s.idx >= len(s.msgs)
+
+    def line_trace( s ):
+      return ""
+
+  class Top( ComponentLevel5 ):
+
+    def construct( s ):
+      s.src  = SimpleTestSourceIfc( [1,2,3,4] )
+      s.sink = SimpleTestSinkIfc( [1,2,3,4] )
+
+      s.connect_pairs(
+        s.src.req,     s.sink.resp,
+      )
+
+    def done( s ):
+      return s.src.done() and s.sink.done()
+
+    def line_trace( s ):
+      return  s.src.line_trace() + " >>> " + s.sink.line_trace()
+
+  _test_model( Top )
