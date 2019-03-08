b1f06e81faeeff0589c99acbfeb97ee475c4f02d
Shunning Jiang <sj634@cornell.edu>
2019-03-08 00:29:34 -0500

    remove outdated cl/ifcs code

--- pclib/cl/Entry.py
+++
@@ -1,3 +0,0 @@
-from collections import namedtuple
-
-ValidEntry = namedtuple('ValidEntry', 'val msg')
--- pclib/ifcs/__init__.py
+++ pclib/ifcs/__init__.py
@@ -1,12 +1,3 @@
 
 from ValRdyIfc import InValRdyIfc, OutValRdyIfc
-# from EnRdyBundle  import EnRdyBundle
 from valrdy       import valrdy_to_str
-from MemMsg       import MemReqMsg, MemRespMsg, mk_mem_msg_type
-# from EnqIfcs      import EnqIfcRTL, EnqIfcCL, EnqIfcFL
-# from DeqIfcs      import DeqIfcRTL, DeqIfcCL, DeqIfcFL
-# from MemIfcs      import MemIfcRTL, MemIfcCL, MemIfcFL
-# from DeqEnqAdapters import DeqIfcRTL_EnqIfcRTL, DeqIfcRTL_EnqIfcCL, \
-                           # DeqIfcCL_EnqIfcRTL,  DeqIfcCL_EnqIfcCL
-# from EnqEnqAdapters import *
-# from MemIfcAdapters import MemIfcAdapter
