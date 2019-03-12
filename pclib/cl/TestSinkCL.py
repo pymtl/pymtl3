444fcd5186c5dd1893562cb675f528769036fb58
Shunning Jiang <sj634@cornell.edu>
2019-03-12 19:47:06 -0400

    pclib.cl: add back CL components

---
+++ pclib/cl/TestSinkCL.py
@@ -0,0 +1,95 @@
+#=========================================================================
+# TestSinkCL.py
+#=========================================================================
+#
+# Author : Shunning Jiang
+# Date   : Mar 12, 2018
+
+from pymtl import *
+from collections import deque
+from StallDelayCL import StallDelayCL
+
+# This simple sink does have a buffer at input
+
+class TestSinkError( Exception ):
+  pass
+
+class TestSimpleSink( ComponentLevel6 ):
+
+  def resp_rdy_( s ):
+    return s.idx < len(s.msgs)
+
+  def resp_( s, v ):
+    s.queue.appendleft(v)
+
+  def construct( s, msgs ):
+    s.msgs  = list( msgs )
+    s.queue = deque( maxlen=1 )
+    s.idx  = 0
+
+    s.resp     = CalleePort( s.resp_ )
+    s.resp_rdy = CalleePort( s.resp_rdy_ )
+
+    s.v = None
+    s.trace_len = len(str(msgs[0]))
+
+    @s.update
+    def up_sink():
+      s.v = None
+
+      if s.queue:
+        msg = s.queue.pop()
+        s.v = msg
+
+        if s.idx >= len(s.msgs):
+          raise TestSinkError( """
+  The test sink received a message that !
+  - sink name    : {}
+  - msg number   : {}
+  - actual msg   : {}
+  """.format( s, s.idx, msg )
+          )
+        else:
+          ref = s.msgs[ s.idx ]
+          s.idx += 1
+
+          if msg != ref:
+            raise TestSinkError( """
+  The test sink received an incorrect message!
+  - sink name    : {}
+  - msg number   : {}
+  - expected msg : {}
+  - actual msg   : {}
+  """.format( s, s.idx, ref, msg )
+          )
+
+    s.add_constraints(
+      U(up_sink) < M(s.resp_    ), # pipe behavior
+      U(up_sink) < M(s.resp_rdy_),
+    )
+
+  def done( s ):
+    return s.idx >= len(s.msgs)
+
+  def line_trace( s ):
+    return "{}".format( "" if s.v is None else str(s.v) ).ljust( s.trace_len )
+
+class TestSink( ComponentLevel6 ):
+
+  def construct( s, msgs, stall_prob=0, delay=1 ):
+
+    s.resp     = CalleePort()
+    s.resp_rdy = CalleePort()
+
+    s.sink = TestSimpleSink( msgs )
+
+    s.stall_delay = StallDelayCL( stall_prob, delay )(
+      recv = s.resp,      recv_rdy = s.resp_rdy,
+      send = s.sink.resp, send_rdy = s.sink.resp_rdy,
+    )
+
+  def done( s ):
+    return s.sink.done()
+
+  def line_trace( s ):
+    return s.stall_delay.line_trace()
