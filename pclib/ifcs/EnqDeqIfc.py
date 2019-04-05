7084b8bf099c00ab5561750bd273bd8e52521078
yo96 <yo96@cornell.edu>
2019-04-05 18:27:31 -0400

    added customized connect for EnqIfcRTL.

--- pclib/ifcs/EnqDeqIfc.py
+++ pclib/ifcs/EnqDeqIfc.py
@@ -8,6 +8,8 @@
 
 from pymtl import *
 from ifcs_utils import enrdy_to_str
+from GuardedIfc import *
+from send_recv_ifc_adapters import RecvCL2SendRTL, RecvRTL2SendCL
 
 #-------------------------------------------------------------------------
 # EnqIfcRTL
@@ -20,6 +22,36 @@ class EnqIfcRTL( Interface ):
     s.msg = InPort ( Type )
     s.en  = InPort ( int if Type is int else Bits1 )
     s.rdy = OutPort( int if Type is int else Bits1 )
+
+    s.MsgType = Type
+
+  def connect( s, other, parent ):
+    if isinstance( other, GuardedCallerIfc ):
+
+      m = RecvCL2SendRTL( s.MsgType )
+      
+      if hasattr( parent, "enq_adapter_cnt" ):
+        cnt = getattr( parent, "enq_adapter_cnt" )
+        setattr( parent, "enq_adapter_" + str( cnt ), m )
+        parent.connect_pairs(
+          other,  m.recv,
+          m.send, s
+        )
+        parent.enq_adapter_cnt += 1
+        return True
+
+      else:
+        setattr( parent, "enq_adapter_0", m )
+        parent.connect_pairs(
+          other,      m.recv,
+          m.send.msg, s.msg,
+          m.send.en,  s.en,
+          m.send.rdy, s.rdy
+        )
+        setattr( parent, "enq_adapter_cnt", 1 )
+        return True
+
+    return False
 
   def line_trace( s ):
     return enrdy_to_str( s.msg, s.en, s.rdy )
@@ -38,6 +70,8 @@ class DeqIfcRTL( Interface ):
     s.msg = OutPort( Type )
     s.en  = InPort ( int if Type is int else Bits1 )
     s.rdy = OutPort( int if Type is int else Bits1 )
+
+    s.MsgType = Type
 
   def line_trace( s ):
     return enrdy_to_str( s.msg, s.en, s.rdy )
