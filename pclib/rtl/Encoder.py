6cf7c9a7f1670657e474c14bf12d4604b3c9e738
yo96 <yo96@cornell.edu>
2019-04-05 15:23:54 -0400

    implemented RTL encoder.

---
+++ pclib/rtl/Encoder.py
@@ -0,0 +1,33 @@
+#=========================================================================
+# Encoder.py
+#=========================================================================
+# A priority encoder for arbitration
+#
+# Author : Yanghui Ou, Cheng Tan
+#   Date : Mar 1, 2019
+
+from pymtl import *
+
+class Encoder( Component ):
+  def construct( s, in_width, out_width ):
+
+    # Interface
+
+    s.in_ =  InVPort( mk_bits( in_width  ) )
+    s.out = OutVPort( mk_bits( out_width ) )
+    
+    # Constants
+
+    s.din_wid  = in_width
+    s.dout_wid = out_width
+
+    # Logic
+
+    @s.update
+    def encode():
+      s.out = 0
+      for i in range( in_width ):
+        s.out = i if s.in_[i] else s.out
+  
+  def line_trace( s ):
+    return "in:{:0>{n}b} | out:{}".format( int( s.in_ ), s.out, n=s.din_wid ) 
---
+++ pclib/rtl/Encoder_test.py
@@ -0,0 +1,38 @@
+#=========================================================================
+# Encoder_test
+#=========================================================================
+# Test for RTL priority encoder.
+#
+# Author : Yanghui Ou
+#   Date : Apr 5, 2019
+
+from pymtl       import *
+from pclib.test  import TestVectorSimulator
+from Encoder     import Encoder 
+
+def run_test( model, test_vectors ):
+
+  def tv_in( model, test_vector ):
+    model.in_ = test_vector[0]
+
+  def tv_out( model, test_vector ):
+    assert model.out == test_vector[1]
+
+  sim = TestVectorSimulator( model, test_vectors, tv_in, tv_out )
+  sim.run_test()
+
+def test_encoder_5_directed( dump_vcd, test_verilog ):
+  model = Encoder( 5, 3 )
+  model.vcd_file = dump_vcd
+  if test_verilog:
+    model = TranslationTool( model )
+
+  run_test( model, [
+    # in                out 
+    [ Bits5( 0b00000 ), 0  ],
+    [ Bits5( 0b00001 ), 0  ],
+    [ Bits5( 0b00010 ), 1  ],
+    [ Bits5( 0b00100 ), 2  ],
+    [ Bits5( 0b01100 ), 3  ],
+    [ Bits5( 0b10000 ), 4  ]
+  ] )
