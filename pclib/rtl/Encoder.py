e4b8a35e5ff92c8959c13229252db44f0ba8afe7
yo96 <yo96@cornell.edu>
2019-04-06 14:07:04 -0400

    Encoder: cleaned up argument names.

--- pclib/rtl/Encoder.py
+++ pclib/rtl/Encoder.py
@@ -9,25 +9,27 @@
 from pymtl import *
 
 class Encoder( Component ):
-  def construct( s, in_width, out_width ):
+  def construct( s, in_nbits, out_nbits ):
 
     # Interface
 
-    s.in_ =  InPort( mk_bits( in_width  ) )
-    s.out = OutPort( mk_bits( out_width ) )
+    s.in_ =  InPort( mk_bits( in_nbits  ) )
+    s.out = OutPort( mk_bits( out_nbits ) )
     
     # Constants
 
-    s.din_wid  = in_width
-    s.dout_wid = out_width
+    s.in_nbits  = in_nbits
+    s.out_nbits = out_nbits
 
     # Logic
 
     @s.update
     def encode():
       s.out = 0
-      for i in range( in_width ):
+      for i in range( s.in_nbits ):
         s.out = i if s.in_[i] else s.out
   
   def line_trace( s ):
-    return "in:{:0>{n}b} | out:{}".format( int( s.in_ ), s.out, n=s.din_wid ) 
+    return "in:{:0>{n}b} | out:{}".format( 
+      int( s.in_ ), s.out, n=s.in_nbits 
+    )
