de8e2e7bb13377d85c927cd3b20862c9d6c53729
yo96 <yo96@cornell.edu>
2019-04-06 18:38:51 -0400

    avoid calling super.

--- pymtl/dsl/Component.py
+++ pymtl/dsl/Component.py
@@ -20,13 +20,26 @@ class Component( ComponentLevel6 ):
       s.clk   = InPort( Bits1 )
       s.reset = InPort( Bits1 )
       
-      super( Component, s )._construct()
+      kwargs = s._dsl.kwargs.copy()
+      if "elaborate" in s._dsl.param_dict:
+        kwargs.update( { x: y for x, y in s._dsl.param_dict[ "elaborate" ].iteritems()
+                              if x } )
+
+      s._handle_guard_methods()
 
       try:
         s.connect( s.clk, s.get_parent_object().clk )
         s.connect( s.reset, s.get_parent_object().reset )
       except AttributeError:
         pass
+
+      # Same as parent class _construct
+      s.construct( *s._dsl.args, **kwargs )
+
+      if s._dsl.call_kwargs is not None: # s.a = A()( b = s.b )
+        s._continue_call_connect()
+
+      s._dsl.constructed = True
 
   def sim_reset( s ):
     assert s._dsl.elaborate_top is s # assert sim_reset is top
