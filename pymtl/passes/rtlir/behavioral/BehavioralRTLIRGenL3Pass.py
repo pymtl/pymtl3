f048e914a5f2d1b1c09f19af5ef8f5aec4285b6f
Peitian Pan <pp482@cornell.edu>
2019-05-16 21:03:12 -0400

    Avoid using inspect

--- pymtl/passes/rtlir/behavioral/BehavioralRTLIRGenL3Pass.py
+++ pymtl/passes/rtlir/behavioral/BehavioralRTLIRGenL3Pass.py
@@ -6,8 +6,6 @@
 """Provide L3 behavioral RTLIR generation pass."""
 from __future__ import absolute_import, division, print_function
 
-import inspect
-
 import pymtl
 from pymtl.passes import BasePass
 from pymtl.passes.BasePass import PassMetadata
@@ -17,15 +15,11 @@ from .errors import PyMTLSyntaxError
 
 
 class BehavioralRTLIRGenL3Pass( BasePass ):
-
   def __call__( s, m ):
     """ generate RTLIR for all upblks of m """
-
     if not hasattr( m, '_pass_behavioral_rtlir_gen' ):
       m._pass_behavioral_rtlir_gen = PassMetadata()
-
     m._pass_behavioral_rtlir_gen.rtlir_upblks = {}
-
     visitor = BehavioralRTLIRGeneratorL3( m )
 
     upblks = {
@@ -40,9 +34,7 @@ class BehavioralRTLIRGenL3Pass( BasePass ):
           visitor.enter( blk, m.get_update_block_ast( blk ) )
 
 class BehavioralRTLIRGeneratorL3( BehavioralRTLIRGeneratorL2 ):
-
   def __init__( s, component ):
-
     super( BehavioralRTLIRGeneratorL3, s ).__init__( component )
 
   def visit_Call( s, node ):
@@ -51,30 +43,20 @@ class BehavioralRTLIRGeneratorL3( BehavioralRTLIRGeneratorL2 ):
     At L3 we need to support the syntax of struct instantiation in upblks.
     This is achieved by function calls like `struct( 1, 2, 0 )`.
     """
-
     obj = s.get_call_obj( node )
-
-    if ( inspect.isclass( obj ) ) and \
-        ( not issubclass(obj, pymtl.Bits) ) and ( not obj is bool ):
-
+    if isinstance(obj, type) and not issubclass(obj, pymtl.Bits) and obj is not bool:
       if node.args:
         raise PyMTLSyntaxError(
           s.blk, node, 'only keyword args are accepted by struct instantiation!'
         )
-
       keywords = []
       kwargs = []
-
       for keyword in node.keywords:
-
         keywords.append( keyword.arg )
         kwargs.append( s.visit( keyword.value ) )
-
       ret = StructInst( obj, keywords, kwargs )
       ret.ast = node
-
       return ret
 
     else:
-
       return super( BehavioralRTLIRGeneratorL3, s ).visit_Call( node )
