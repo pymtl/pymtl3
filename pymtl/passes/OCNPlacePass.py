17b521b9bc83419b253dd3e1a08e62bbbc6f6c83
tancheng <tancheng1990@gmail.com>
2019-05-20 11:54:46 -0400

    bypass physical pass when physical_elaborate() no defined

--- pymtl/passes/OCNPlacePass.py
+++ pymtl/passes/OCNPlacePass.py
@@ -18,6 +18,7 @@ class OCNPlacePass( BasePass ):
     if not hasattr( top, "elaborate_physical" ):
       print( "Note that there is no elaborate_physical defined in "
              "network module:{}".format(top.__class__.__name__) )
+      return
 
     all_components = sorted( top.get_all_components(), key=repr )
     all_components.reverse()
@@ -29,9 +30,11 @@ class OCNPlacePass( BasePass ):
     # Generate the floorplanning script
 
     print '\n---- placement geometric information ----'
+    print "createFence Ring4 {} {} {} {}\n".format( 
+            top.dim.x, top.dim.y, top.dim.x + top.dim.w, top.dim.y + top.dim.h)
     for i, r in enumerate( top.routers ):
       print "createFence router__{} {} {} {} {}".format(
-        str(i), r.dim.x, r.dim.y, r.dim.w, r.dim.h )
+        str(i), r.dim.x, r.dim.y, r.dim.x + r.dim.w, r.dim.y + r.dim.h )
 
     print '---------------------'
 
