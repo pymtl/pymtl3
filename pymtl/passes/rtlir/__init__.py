c2b674ff8ba5556b64134767167e9b68f47b39c6
Peitian Pan <pp482@cornell.edu>
2019-05-24 16:57:03 -0400

    Remove comments

--- pymtl/passes/rtlir/__init__.py
+++ pymtl/passes/rtlir/__init__.py
@@ -5,9 +5,6 @@ to RTLIR representation.
 """
 from __future__ import absolute_import, division, print_function
 
-# Expose modules containing class definitions that might be interesting
-# to some users
-# Expose passes
 from .behavioral import (
     BehavioralRTLIR,
     BehavioralRTLIRGenPass,
@@ -16,6 +13,5 @@ from .behavioral import (
 )
 from .rtype import RTLIRDataType, RTLIRType
 from .rtype.RTLIRDataType import get_rtlir_dtype
-# Expose methods that generate RTLIR for a single object
 from .rtype.RTLIRType import get_rtlir, is_rtlir_convertible
 from .structural import StructuralRTLIRGenPass, StructuralRTLIRSignalExpr
