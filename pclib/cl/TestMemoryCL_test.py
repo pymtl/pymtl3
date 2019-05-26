4b1ea79859eae19d512aeaadcef588170e89e3bd
yo96 <yo96@cornell.edu>
2019-05-26 18:13:42 -0400

    pass in bits instead of integers.

--- pclib/cl/TestMemoryCL_test.py
+++ pclib/cl/TestMemoryCL_test.py
@@ -78,12 +78,13 @@ resp_type_dict = {
 }
 
 req_cls, resp_cls = mk_mem_msg( 8, 32, 32 )
+b32 = Bits32
 
 def req( type_, opaque, addr, len, data ):
-  return req_cls( req_type_dict[type_], opaque, addr, len, data)
+  return req_cls( req_type_dict[type_], opaque, addr, len, b32(data) )
 
 def resp( type_, opaque, len, data ):
-  return resp_cls( resp_type_dict[type_], opaque, 0, len, data)
+  return resp_cls( resp_type_dict[type_], opaque, 0, len, b32(data) )
 
 #----------------------------------------------------------------------
 # Test Case: basic
