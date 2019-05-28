474924ad8a4d9a4040b1eac0e6b270090b0dea4d
Shunning Jiang <sj634@cornell.edu>
2019-05-28 10:32:12 -0400

    comment out all __init__ to clean up files one by one

--- pymtl3/dsl/__init__.py
+++ pymtl3/dsl/__init__.py
@@ -4,25 +4,26 @@ to users in pymtl/__init__.py
 """
 from __future__ import absolute_import, division, print_function
 
-from .Component import Component
-from .ComponentLevel1 import ComponentLevel1
-from .ComponentLevel2 import ComponentLevel2
-from .ComponentLevel3 import ComponentLevel3
-from .ComponentLevel4 import ComponentLevel4
-from .ComponentLevel5 import ComponentLevel5
-from .ComponentLevel6 import ComponentLevel6, non_blocking
-from .Connectable import (
-    CalleePort,
-    CallerPort,
-    Const,
-    InPort,
-    Interface,
-    MethodPort,
-    NonBlockingCalleeIfc,
-    NonBlockingCallerIfc,
-    NonBlockingInterface,
-    OutPort,
-    Signal,
-    Wire,
-)
-from .NamedObject import NamedObject
+# from .Component import Component
+# from .ComponentLevel1 import ComponentLevel1
+# from .ComponentLevel2 import ComponentLevel2
+# from .ComponentLevel3 import ComponentLevel3
+# from .ComponentLevel4 import ComponentLevel4
+# from .ComponentLevel5 import ComponentLevel5
+# from .ComponentLevel6 import ComponentLevel6, non_blocking
+# from .Connectable import (
+    # CalleePort,
+    # CallerPort,
+    # Const,
+    # InPort,
+    # Interface,
+    # MethodPort,
+    # NonBlockingCalleeIfc,
+    # NonBlockingCallerIfc,
+    # NonBlockingInterface,
+    # OutPort,
+    # Signal,
+    # Wire,
+# )
+# from .ConstraintTypes import U, M, RD, WR
+# from .NamedObject import NamedObject
