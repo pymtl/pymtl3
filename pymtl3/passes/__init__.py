e3aae010a3b5829c6d3be2f33e1d3e352ef6a717
Shunning Jiang <sj634@cornell.edu>
2019-05-28 22:10:39 -0400

    expose existing pass groups

--- pymtl3/passes/__init__.py
+++ pymtl3/passes/__init__.py
@@ -1,5 +1,3 @@
 from __future__ import absolute_import, division, print_function
 
-# from .BasePass import BasePass
-# from .GenDAGPass import GenDAGPass
-# from .PassGroups import *
+from .PassGroups import *
