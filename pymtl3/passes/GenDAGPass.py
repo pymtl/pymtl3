9cdda9ce6d15f92ac4bc8da030644a81e2ed5251
Shunning Jiang <sj634@cornell.edu>
2019-05-28 21:43:31 -0400

    fix a bug in GenDAGPass

--- pymtl3/passes/GenDAGPass.py
+++ pymtl3/passes/GenDAGPass.py
@@ -12,6 +12,7 @@ from __future__ import absolute_import, division, print_function
 
 from collections import defaultdict, deque
 
+from pymtl3.datatypes import *
 from pymtl3.dsl import *
 
 from .BasePass import BasePass, PassMetadata
