bd88742c9ca2d9f3430bffff66ccc792de2cf0b9
yo96 <yo96@cornell.edu>
2019-06-12 23:33:15 -0400

    checksum RTL.

---
+++ tutorials/01_checksum_unit/ChecksumRTL_test.py
@@ -0,0 +1,63 @@
+"""
+==========================================================================
+ ChecksumRTL_test.py
+==========================================================================
+Test cases for CL checksum unit.
+
+Author : Yanghui Ou
+  Date : June 6, 2019
+"""
+from __future__ import absolute_import, division, print_function
+
+from pymtl3 import *
+from pymtl3.passes import DynamicSim
+from pymtl3.stdlib.cl.queues import BypassQueueCL
+from pymtl3.stdlib.test import TestSinkCL, TestSrcCL
+
+from ChecksumCL_test import FuncTestsScycleCL, SrcSinkTestsScycleCL, TestHarness
+from ChecksumFL import b128_to_words, checksum, words_to_b128
+from ChecksumRTL import ChecksumRTL
+
+#-------------------------------------------------------------------------
+# Wrap RTL checksum unit into a function
+#-------------------------------------------------------------------------
+
+def checksum_rtl( words ):
+  bits_in = words_to_b128( words )
+
+  dut = ChecksumRTL()
+  dut.apply( DynamicSim )
+
+  dut.sim_reset()
+  dut.send.rdy = b1(1)
+  while not dut.recv.rdy:
+    dut.tick()
+
+  dut.recv.en = b1(1)
+  dut.recv.msg = bits_in
+  dut.tick()
+
+  while not dut.send.en:
+    dut.tick()
+  
+  return dut.send.msg
+
+#-------------------------------------------------------------------------
+# Functionality test
+#-------------------------------------------------------------------------
+
+class FuncTestsRTL( FuncTestsScycleCL ):
+  
+  def func_impl( s, words ):
+    return checksum_rtl( words )
+
+#-------------------------------------------------------------------------
+# Src/sink based tests
+#-------------------------------------------------------------------------
+
+class SrcSinkTestsRTL( SrcSinkTestsScycleCL ):
+
+  @classmethod
+  def setup_class( cls ):
+    cls.DutType = ChecksumRTL
+    cls.nstages = None
