af184c0de231680751455619b0bc4f318d5e4eeb
yo96 <yo96@cornell.edu>
2019-06-16 17:36:03 -0400

    refactored cl xcel to have the same timing behavior as the rtl xcel.

--- examples/ex04_xcel/ChecksumXcelCL.py
+++ examples/ex04_xcel/ChecksumXcelCL.py
@@ -10,7 +10,7 @@ Author : Yanghui Ou
 from __future__ import absolute_import, division, print_function
 
 from pymtl3 import *
-from pymtl3.stdlib.cl.queues import BypassQueueCL
+from pymtl3.stdlib.cl.queues import NormalQueueCL, BypassQueueCL
 from pymtl3.stdlib.ifcs import mk_xcel_msg, XcelMsgType
 from pymtl3.stdlib.ifcs.xcel_ifcs import XcelMinionIfcCL
 
@@ -18,6 +18,30 @@ from examples.ex02_cksum.ChecksumCL import ChecksumCL
 from examples.ex02_cksum.ChecksumRTL import ChecksumRTL
 from examples.ex02_cksum.utils import words_to_b128
 
+#-------------------------------------------------------------------------
+# A piece of CL logic that converts Bits32 to a response message
+#-------------------------------------------------------------------------
+
+class CombLogicCL( Component ):
+
+  def construct( s ):
+    _, Resp = mk_xcel_msg( 5, 32 )
+    s.Resp = Resp
+    s.data = None 
+
+    s.add_constraints( M(s.write) < M(s.read) )
+  
+  @method_port
+  def write( data ):
+    s.data = b32(data)
+
+  @method_port
+  def read( type_ ):
+    return s.Resp( b1(type_), s.data ) 
+
+#-------------------------------------------------------------------------
+# A piece of CL logic that converts Bits32 to a response message
+#-------------------------------------------------------------------------
 
 class ChecksumXcelCL( Component ):
 
@@ -26,58 +50,80 @@ class ChecksumXcelCL( Component ):
     # Interface
 
     ReqType, RespType = mk_xcel_msg( 5, 32 )
-    s.xcel = XcelMinionIfcCL( ReqType, RespType, s.req, s.req_rdy )
-
     s.RespType = RespType
+
+    s.xcel = XcelMinionIfcCL( ReqType, RespType )
+
+    # State encoding
+
+    s.XCFG = 0
+    s.WAIT = 1
+    s.BUSY = 2
+
+    # Local paramters
+
+    RD = XcelMsgType.READ
+    WR = XcelMsgType.WRITE
 
     # Components
 
+    s.in_q = NormalQueueCL( num_entries=2 )
     s.reg_file = [ b32(0) for _ in range(6) ]
-    s.busy = False
-    s.resp_msg = None
-
     s.checksum_unit = ChecksumCL()
+
+    s.busy = False
+    s.state = s.XCFG
+
+    s.connect( s.xcel.req, s.in_q.enq )
     
     # TODO: replace out_q with a combinational adapter
     s.out_q = BypassQueueCL( num_entries=1 )
     s.connect( s.checksum_unit.send, s.out_q.enq )
 
     @s.update
-    def up_start():
-      if s.reg_file[4]:
-        s.busy = True
-        s.reg_file[4] = b32(0)
+    def up_tick():
+      if s.state == s.XCFG:
+        # Dequeue a request message from input queue and send response.
+        if s.in_q.deq.rdy() and s.xcel.resp.rdy():
+          req = s.in_q.deq()
+          if req.type_ == RD:
+            s.xcel.resp( s.RespType( RD, s.reg_file[ int(req.addr) ] ) )
+          elif req.type_ == XcelMsgType.WRITE:
+            s.reg_file[ int(req.addr) ] = req.data
+            s.xcel.resp( s.RespType( WR, 0 ) )
+            
+            # If the go bit is written
+            if req.addr == 4: 
+              if s.checksum_unit.recv.rdy():
+                s.state = s.BUSY
+                words = []
+                for i in range( 4 ):
+                  words.append( s.reg_file[i][0 :16] )
+                  words.append( s.reg_file[i][16:32] )
+                s.checksum_unit.recv( words_to_b128( words ) )
+              else:
+                s.state = s.WAIT
 
-        words = []
-        for i in range( 4 ):
-          words.append( s.reg_file[i][0 :16] )
-          words.append( s.reg_file[i][16:32] )
+      elif s.state == s.WAIT:
+        if s.checksum_unit.recv.rdy():
+          s.state == s.BUSY
+          words = []
+          for i in range( 4 ):
+            words.append( s.reg_file[i][0 :16] )
+            words.append( s.reg_file[i][16:32] )
+          s.checksum_unit.recv( words_to_b128( words ) )
+          s.state = s.BUSY
 
-        s.checksum_unit.recv( words_to_b128( words ) )
-
-    @s.update
-    def up_resp():
-      if s.resp_msg is not None and s.xcel.resp.rdy(): 
-        s.xcel.resp( s.resp_msg )
-        s.resp_msg = None
-
-      if s.out_q.deq.rdy():
-        s.reg_file[5] = s.out_q.deq()
-        s.busy = False
-
-    s.add_constraints( U( up_start ) < M( s.xcel.req     ) )
-    s.add_constraints( U( up_resp  ) > M( s.xcel.req.rdy ) )
-
-  def req( s, msg ):
-    if msg.type_ == XcelMsgType.READ:
-      s.resp_msg = s.RespType( XcelMsgType.READ, s.reg_file[ int(msg.addr) ] )
-
-    elif msg.type_ == XcelMsgType.WRITE:
-      s.reg_file[ int(msg.addr) ] = msg.data
-      s.resp_msg = s.RespType( XcelMsgType.WRITE, 0 )
-
-  def req_rdy( s ):
-    return not s.busy 
+      else: # s.state == s.BUSY
+        if s.out_q.deq.rdy():
+          s.reg_file[5] = s.out_q.deq()
+          s.state = s.XCFG
 
   def line_trace( s ):
-    return "{}({}){}".format( s.xcel.req, '#' if s.busy else ' ', s.xcel.resp )
+    state_str = (
+      "XCFG" if s.state == s.XCFG else
+      "WAIT" if s.state == s.WAIT else
+      "BUSY" if s.state == s.BUSY else
+      "XXXX"
+    )
+    return "{}({}){}".format( s.xcel.req, state_str, s.xcel.resp )
