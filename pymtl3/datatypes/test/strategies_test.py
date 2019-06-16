337caf64678e8c1466421b0fdf9a112aefe8d2eb
yo96 <yo96@cornell.edu>
2019-06-15 22:13:24 -0400

    added bits strategy.

---
+++ pymtl3/datatypes/test/strategies_test.py
@@ -0,0 +1,42 @@
+"""
+==========================================================================
+strategies.py
+==========================================================================
+Hypothesis strategies for built-in data types.
+
+Author : Yanghui Ou
+  Date : June 16, 2019
+"""
+from __future__ import absolute_import, division, print_function
+
+import pytest
+import hypothesis
+from hypothesis import strategies as st
+
+from pymtl3.datatypes.bits_import import *
+from pymtl3.datatypes.strategies import bits_strat, signed_bits_strat
+
+@pytest.mark.parametrize( 'nbits', [3, 4, 8, 16, 32] )
+def test_unsiged_pro( nbits ):
+  print("")
+  @hypothesis.given(
+    bits = bits_strat(nbits)
+  )
+  @hypothesis.settings( max_examples=16 )
+  def actual_test( bits ):
+    assert bits.nbits == nbits
+    print( bits, bits.uint() )
+  actual_test()
+
+@pytest.mark.parametrize( 'nbits', [1, 3, 4, 8, 16, 32] )
+def test_signed_pro( nbits ):
+  print("")
+  @hypothesis.given(
+    bits = bits_strat(nbits, True)
+  )
+  @hypothesis.settings( max_examples=16 )
+  def actual_test( bits ):
+    assert bits.nbits == nbits
+    print
+    print( bits, bits.int() )
+  actual_test()
