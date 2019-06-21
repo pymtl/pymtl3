e8318f9df233d5f7ae6d1219f46d9ed0755b4645
Christopher Batten <cbatten@cornell.edu>
2019-06-21 15:20:00 -0400

    working on checkum RTL

--- examples/ex02_cksum/ChecksumRTL.py
+++ examples/ex02_cksum/ChecksumRTL.py
@@ -24,18 +24,17 @@ class StepUnit( Component ):
     # Interface
 
     s.word_in  = InPort ( Bits16 )
-    s.sum1_acc = InPort ( Bits32 )
-    s.sum1_out = OutPort( Bits32 ) 
-
-    s.sum2_acc = InPort ( Bits32 )
+    s.sum1_in  = InPort ( Bits32 )
+    s.sum2_in  = InPort ( Bits32 )
+    s.sum1_out = OutPort( Bits32 )
     s.sum2_out = OutPort( Bits32 )
 
     # Logic
 
     @s.update
     def up_step():
-     s.sum1_out = ( b32(s.word_in) + s.sum1_acc ) & b32(0xffff)
-     s.sum2_out = ( s.sum1_out     + s.sum2_acc ) & b32(0xffff)
+      s.sum1_out = ( b32(s.word_in) + s.sum1_in ) & b32(0xffff)
+      s.sum2_out = ( s.sum1_out     + s.sum2_in ) & b32(0xffff)
 
 #-------------------------------------------------------------------------
 # ChecksumRTL
@@ -47,17 +46,17 @@ class ChecksumRTL( Component ):
 
     # Interface
 
-    s.recv = RecvIfcRTL( Bits128 )
-    s.send = SendIfcRTL( Bits32  )
+    s.recv  = RecvIfcRTL( Bits128 )
+    s.send  = SendIfcRTL( Bits32  )
 
     # Component
 
-    s.words  = [ Wire( Bits16 ) for _ in range( 8 ) ]
-    s.sum1   = Wire( Bits32 )
-    s.sum2   = Wire( Bits32 )
+    s.words = [ Wire( Bits16 ) for _ in range( 8 ) ]
+    s.sum1  = Wire( Bits32 )
+    s.sum2  = Wire( Bits32 )
 
-    s.in_q = PipeQueueRTL( Bits128, num_entries=1 )
-    s.steps        = [ StepUnit() for _ in range( 8 ) ]
+    s.in_q  = PipeQueueRTL( Bits128, num_entries=1 )
+    s.steps = [ StepUnit() for _ in range( 8 ) ]
 
     # Register input
 
@@ -67,17 +66,17 @@ class ChecksumRTL( Component ):
 
     for i in range( 8 ):
       s.connect( s.words[i], s.in_q.deq.msg[i*16:(i+1)*16] )
-    
+
     # Connect step units
 
     for i in range( 8 ):
       s.connect( s.steps[i].word_in, s.words[i] )
       if i == 0:
-        s.connect( s.steps[i].sum1_acc, b32(0) )
-        s.connect( s.steps[i].sum2_acc, b32(0) )
+        s.connect( s.steps[i].sum1_in, b32(0) )
+        s.connect( s.steps[i].sum2_in, b32(0) )
       else:
-        s.connect( s.steps[i].sum1_acc, s.steps[i-1].sum1_out )
-        s.connect( s.steps[i].sum2_acc, s.steps[i-1].sum2_out )
+        s.connect( s.steps[i].sum1_in, s.steps[i-1].sum1_out )
+        s.connect( s.steps[i].sum2_in, s.steps[i-1].sum2_out )
     s.connect( s.sum1, s.steps[-1].sum1_out )
     s.connect( s.sum2, s.steps[-1].sum2_out )
 
@@ -89,6 +88,6 @@ class ChecksumRTL( Component ):
     @s.update
     def up_rtl_sum():
       s.send.msg = ( s.sum2 << 16 ) | s.sum1
-  
+
   def line_trace( s ):
     return "{}(){}".format( s.recv, s.send )
