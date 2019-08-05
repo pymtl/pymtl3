a27fb2cc2d31d6e10e97ad82a8ba8cece1265473
Peitian Pan <pp482@cornell.edu>
2019-08-05 09:03:38 -0400

    Fix typo in backend directed tests

--- pymtl3/passes/yosys/test/TranslationImport_closed_loop_directed_test.py
+++ pymtl3/passes/yosys/test/TranslationImport_closed_loop_directed_test.py
@@ -6,22 +6,22 @@
 """Closed-loop test with SystemVerilog translation and import."""
 
 from pymtl3.passes import DynamicSim
-from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_dircted_test import (
+from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_directed_test import (
     test_bypass_queue as _bypass_queue,
 )
-from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_dircted_test import (
+from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_directed_test import (
     test_bypass_queue_stall as _bypass_queue_stall,
 )
-from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_dircted_test import (
+from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_directed_test import (
     test_normal_queue as _normal_queue,
 )
-from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_dircted_test import (
+from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_directed_test import (
     test_normal_queue_stall as _normal_queue_stall,
 )
-from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_dircted_test import (
+from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_directed_test import (
     test_pipe_queue as _pipe_queue,
 )
-from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_dircted_test import (
+from pymtl3.passes.sverilog.test.TranslationImport_closed_loop_directed_test import (
     test_pipe_queue_stall as _pipe_queue_stall,
 )
 from pymtl3.passes.yosys import ImportConfigs, ImportPass, TranslationPass
