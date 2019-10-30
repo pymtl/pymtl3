ff1bcd45a32e337c97233ab970f5a70d642f7252
tancheng <tancheng1990@gmail.com>
2019-10-30 15:29:48 -0400

    initially implement grid and automatically calculate dimension information recursively

--- pymtl3/passes/Grid.py
+++ pymtl3/passes/Grid.py
@@ -17,12 +17,63 @@ class Grid( object ):
     s.row_id = row_id
     s.col_id = col_id
     s.component = None
+    s.x_ratio = 0.0
+    s.y_ratio = 0.0
+    s.w_ratio = 1.0
+    s.h_ratio = 1.0
+    s.dim_x = 0
+    s.dim_y = 0
+    s.dim_w = 0
+    s.dim_h = 0
 
   def divide( s, rows, cols ):
     s.sub_grids = [ [ Grid(i,j,s) for j in range(cols) ]
                       for i in range(rows) ]
+    w_ratio = s.w_ratio/cols
+    h_ratio = s.h_ratio/cols
+    for r in range( rows ):
+      for c in range( cols ):
+        s.sub_grids[r][c].w_ratio = w_ratio
+        s.sub_grids[r][c].h_ratio = h_ratio
+        s.sub_grids[r][c].x_ratio = s.x_ratio + c*w_ratio
+        s.sub_grids[r][c].y_ratio = s.y_ratio + r*h_ratio
     return s.sub_grids
 
   def setComponent( s, component ):
     s.component = component
+    max_w = 0
+    max_h = 0
+    if hasattr( component, "dim_w" ):
+      max_w = component.dim_w
+    if hasattr( component, "dim_h" ):
+      max_h = component.dim_h
+    print( "now in ", component.component_name )
+    if hasattr( component, "sub_grids" ):
+      print( "there is sub_grids..." )
+      for r in range( len( component.sub_grids ) ):
+        for c in range( len( component.sub_grids[r] ) ):
+          temp_w = component.sub_grids[r][c].dim_w
+          temp_h = component.sub_grids[r][c].dim_h
+          if temp_w > max_w:
+            max_w = temp_w
+          if temp_h > max_h:
+            max_h = temp_h
+      sub_grids_rows = len( component.sub_grids    )
+      sub_grids_cols = len( component.sub_grids[0] )
+      component.dim_w = sub_grids_cols * max_w
+      component.dim_h = sub_grids_rows * max_h
+      s.dim_w = component.dim_w
+      s.dim_h = component.dim_h
+      print( "see w: ", component.dim_w, "; cols: ", sub_grids_cols, "; max_w: ", max_w )
+      print( "see h: ", component.dim_h, "; rows: ", sub_grids_rows, "; max_h: ", max_h )
+    else:
+      s.dim_w = component.dim_w
+      s.dim_h = component.dim_h
+      print( "no sub_grids..." )
+
+  def calculateAbsoluteDimInfo( s ):
+    # ...
+    pass
+
+
 
--- pymtl3/passes/test/PlacementPass_test.py
+++ pymtl3/passes/test/PlacementPass_test.py
@@ -16,10 +16,12 @@ class Wall( Component ):
     s.wall_id = wall_id
 
   def place( s, grid ):
+    # assume this is the most fine-granularity component
+    s.component_name = "wall"
     s.dim_x = 0
     s.dim_y = 0
     s.dim_w = 40
-    s.dim_h = 40
+    s.dim_h = 20
     grid.setComponent( s )
 
 class SmallRoom( Component ):
@@ -27,6 +29,8 @@ class SmallRoom( Component ):
     s.room_id = room_id
 
   def place( s, grid ):
+    # assume this is the most fine-granularity component
+    s.component_name = "small room"
     s.dim_x = 0
     s.dim_y = 0
     s.dim_w = 10
@@ -38,6 +42,8 @@ class BigRoom( Component ):
     s.room_id = room_id
 
   def place( s, grid ):
+    # assume this is the most fine-granularity component
+    s.component_name = "big room"
     s.dim_x = 0
     s.dim_y = 0
     s.dim_w = 20
@@ -54,23 +60,78 @@ class House( Component ):
     s.small_rooms = [ SmallRoom( i ) for i in range( small_room_count ) ]
 
   def place( s, grid ):
-    sub_grids = grid.divide( rows = 3, cols = 1 )
+    s.component_name = "house"
+    s.sub_grids = grid.divide( rows = 3, cols = 1 )
 
-    big_room_grids   = sub_grids[0][0].divide( rows=1,
-                       cols=s.big_room_count )
-    small_room_grids = sub_grids[2][0].divide( rows=1,
-                       cols=s.small_room_count )
+    big_room_grids   = s.sub_grids[0][0].divide( rows = 1,
+                       cols = s.big_room_count )
+    wall_grids       = s.sub_grids[1]
+    small_room_grids = s.sub_grids[2][0].divide( rows = 1,
+                       cols = s.small_room_count )
 
     for i in range( s.big_room_count ):
       s.big_rooms[i].place( big_room_grids[0][i] )
 
-    s.walls[0].place( sub_grids[1][0] )
+    s.walls[0].place( wall_grids[0] )
+
+    for i in range( s.small_room_count ):
+      s.small_rooms[i].place( small_room_grids[0][i] )
+
+    print()
+    print( "============ grid ratio ==============" )
+    for j in range( s.big_room_count ):
+      print( "[grid] big room x[", j, "]: ", big_room_grids[0][j].x_ratio )
+      print( "[grid] big room y[", j, "]: ", big_room_grids[0][j].y_ratio )
+      print( "[grid] big room w[", j, "]: ", big_room_grids[0][j].w_ratio )
+      print( "[grid] big room h[", j, "]: ", big_room_grids[0][j].h_ratio )
+      print( "--------------------------" )
+
+    for j in range( s.wall_count ):
+      print( "[grid] wall x[", j, "]: ", wall_grids[j].x_ratio )
+      print( "[grid] wall y[", j, "]: ", wall_grids[j].y_ratio )
+      print( "[grid] wall w[", j, "]: ", wall_grids[j].w_ratio )
+      print( "[grid] wall h[", j, "]: ", wall_grids[j].h_ratio )
+
+    print( "--------------------------" )
 
     for j in range( s.small_room_count ):
-      s.small_rooms[j].place( small_room_grids[0][j] )
+      print( "[grid] small room x[", j, "]: ", small_room_grids[0][j].x_ratio )
+      print( "[grid] small room y[", j, "]: ", small_room_grids[0][j].y_ratio )
+      print( "[grid] small room w[", j, "]: ", small_room_grids[0][j].w_ratio )
+      print( "[grid] small room h[", j, "]: ", small_room_grids[0][j].h_ratio )
+      print( "--------------------------" )
+
+    print( "============ component dim ==============" )
+    for j in range( s.big_room_count ):
+      print( "[dim] big room x[", j, "]: ", s.big_rooms[j].dim_x )
+      print( "[dim] big room y[", j, "]: ", s.big_rooms[j].dim_y )
+      print( "[dim] big room w[", j, "]: ", s.big_rooms[j].dim_w )
+      print( "[dim] big room h[", j, "]: ", s.big_rooms[j].dim_h )
+      print( "--------------------------" )
+
+    for j in range( s.wall_count ):
+      print( "[dim] wall x[", j, "]: ", s.walls[j].dim_x )
+      print( "[dim] wall y[", j, "]: ", s.walls[j].dim_y )
+      print( "[dim] wall w[", j, "]: ", s.walls[j].dim_w )
+      print( "[dim] wall h[", j, "]: ", s.walls[j].dim_h )
+
+    print( "--------------------------" )
+
+    for j in range( s.small_room_count ):
+      print( "[dim] small room x[", j, "]: ", s.small_rooms[j].dim_x )
+      print( "[dim] small room y[", j, "]: ", s.small_rooms[j].dim_y )
+      print( "[dim] small room w[", j, "]: ", s.small_rooms[j].dim_w )
+      print( "[dim] small room h[", j, "]: ", s.small_rooms[j].dim_h )
+      print( "--------------------------" )
 
     grid.setComponent( s )
-    
+
+    print( "============= top =============" )
+    print( "[top] house x: ", grid.dim_x )
+    print( "[top] house y: ", grid.dim_y )
+    print( "[top] house w: ", grid.dim_w )
+    print( "[top] house h: ", grid.dim_h )
+
 def test_place():
   model = House( 4, 1, 4 )
   model.elaborate()
