419d0b789d83ed654a4e7e7b6651329e96e945b8
Shunning Jiang <sj634@cornell.edu>
2019-12-09 14:35:02 -0500

    Revert useless copy-past in strategies

--- pymtl3/datatypes/strategies.py
+++ pymtl3/datatypes/strategies.py
@@ -17,24 +17,7 @@ from .bits_import import mk_bits
 #-------------------------------------------------------------------------
 
 @st.composite
-def bits( draw, nbits, signed=False, min_value=None, max_value=None ):
-  if nbits == 1:
-    value = draw( st.booleans() )
-  elif not signed:
-    if min_value is None: min_value = 0
-    if max_value is None: max_value = 2**nbits - 1
-    assert min_value < max_value
-    value = draw( st.integers(min_value, max_value) )
-  else:
-    if min_value is None: min_value = - ( 2 ** (nbits-1) )
-    if max_value is None: max_value = 2**(nbits-1) - 1
-    assert min_value < max_value
-    value = draw( st.integers(min_value, max_value) )
-
-  return mk_bits(nbits)( value )
-
-@st.composite
-def bitstruct( draw, Type ):
+def bits( draw, nbits, signed=False ):
   if nbits == 1:
     value = draw( st.booleans() )
   elif not signed:
@@ -48,48 +31,3 @@ def bitstruct( draw, Type ):
 
   BitsN = mk_bits( nbits )
   return BitsN( value )
-
-#-------------------------------------------------------------------------
-# bits_struct_strategy
-#-------------------------------------------------------------------------
-def bits_struct_strategy( bits_struct_type,
-                          predefined={},
-                          remaining_names=None ):
-
-  field_strategies = {}
-  for name, field_type in bits_struct_type.fields:
-    predefined_field = predefined.get( name, {} )
-    field_strategies[ name ] = get_strategy_from_type(
-        field_type, predefined_field, remaining_names )
-
-  @st.composite
-  def strategy( draw ):
-    new_bits_struct = bits_struct_type()
-    for name, field_type in bits_struct_type.fields:
-      # recursively draw st
-      data = draw( field_strategies[ name ] )
-      setattr( new_bits_struct, name, data )
-    return new_bits_struct
-
-  return strategy()
-
-
-#-------------------------------------------------------------------------
-# get_strategy_from_type
-#-------------------------------------------------------------------------
-def get_strategy_from_type( dtype, predefined={}, remaining_names=None ):
-  if isinstance( predefined, tuple ):
-    assert isinstance( predefined[ 0 ], SearchStrategy )
-    remaining_names.discard( predefined[ 1 ] )
-    return predefined[ 0 ]
-
-  if isinstance( dtype(), Bits ):
-    if predefined:
-      raise TypeError( "Need strategy for Bits type {}".format(
-          repr( dtype ) ) )
-    return bitstype_strategy( dtype() )
-
-  if isinstance( dtype(), BitStruct ):
-    return bits_struct_strategy( dtype, predefined, remaining_names )
-
-  raise TypeError( "Argument strategy for {} not supported".format( dtype ) )
