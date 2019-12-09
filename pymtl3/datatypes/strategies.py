57eac4aa19e35b4337634aaa876288dd877d608a
Shunning Jiang <sj634@cornell.edu>
2019-12-09 11:35:11 -0500

    Revamp Bits strategy and implement Bitstruct strategy

--- pymtl3/datatypes/strategies.py
+++ pymtl3/datatypes/strategies.py
@@ -4,30 +4,96 @@ strategies.py
 ==========================================================================
 Hypothesis strategies for built-in data types.
 
-Author : Yanghui Ou
-  Date : June 16, 2019
+Author : Shunning Jiang, Yanghui Ou
+  Date : Dec 9, 2019
 """
 import hypothesis
 from hypothesis import strategies as st
 
-from .bits_import import mk_bits
+from .bits_import import Bits, mk_bits
+from .bitstructs import is_bitstruct_class
 
 #-------------------------------------------------------------------------
 # A generic bits strategy.
 #-------------------------------------------------------------------------
 
-@st.composite
-def bits( draw, nbits, signed=False ):
-  if nbits == 1:
-    value = draw( st.booleans() )
-  elif not signed:
-    min_value = 0
-    max_value = 2**nbits - 1
-    value = draw( st.integers(min_value, max_value) )
-  else:
-    min_value = - ( 2 ** (nbits-1) )
-    max_value = 2**(nbits-1) - 1
-    value = draw( st.integers(min_value, max_value) )
+# Return the SearchStrategy for Bits_nbits with the support to limit
+# min/max value and setting signedness
 
+def bits( nbits, signed=False, min_value=None, max_value=None ):
   BitsN = mk_bits( nbits )
-  return BitsN( value )
+
+  if (min_value is not None or max_value is not None) and signed:
+    raise ValueError("bits strategy currently doesn't support setting "
+                     "signedness and min/max value at the same time")
+
+  if min_value is None:
+    min_value = (-(2**(nbits-1))) if signed else 0
+  if max_value is None:
+    max_value = (2**(nbits-1)-1)  if signed else (2**nbits - 1)
+
+  strat = st.booleans() if nbits == 1 else st.integers( min_value, max_value )
+
+  @st.composite
+  def strategy_bits( draw ):
+    return BitsN( draw( strat ) )
+
+  # RETURN A STRATEGY INSTEAD OF FUNCTION
+  return strategy_bits()
+
+# Helper function to handle a list of Bits/structs
+def _list( types ):
+  # Bitstruct type construction already guarantees that all elements in
+  # the list have the same type. Hence we just need to use the first type
+  type_ = types[0]
+
+  # a list of types, recursive
+  if isinstance( type_, list ):
+    strat = _list( type_ )
+  # nested bitstruct, RECURSIVE
+  elif is_bitstruct_class( type_ ):
+    strat = bitstruct( type_ )
+  # bits field, directly use bits strategy
+  else:
+    assert issubclass( type_, Bits )
+    strat = bits( type_.nbits, False )
+
+  @st.composite
+  def strategy_list( draw ):
+    return [ draw(strat) for _ in types ]
+
+  return strategy_list()
+
+# Return the SearchStrategy for bitstruct type T
+
+def bitstruct( T ):
+  types = T.__bitstruct_fields__.values()
+
+  # We capture the fields of T inside a list inside closure of the
+  # strategy. For each field, we recursively compose a strategy based on
+  # the type. The type can be a list of types, a Bits type, or a nested
+  # bitstruct. For nested bitstruct, we recursively call the bitstruct(T)
+  # function to construct the strategy
+
+  strats = [ None ] * len( types )
+  for i, type_ in enumerate( types ):
+    # a list of types, call helper function
+    if isinstance( type_, list ):
+      strats[i] = _list( type_ )
+    # nested bitstruct, RECURSIVE
+    elif is_bitstruct_class( type_ ):
+      strats[i] = bitstruct( type_ )
+    # bits field, directly use bits strategy
+    else:
+      assert issubclass( type_, Bits )
+      strats[i] = bits( type_.nbits, False )
+    assert strats[i] is not None
+
+  @st.composite
+  def strategy_bitstruct( draw ):
+    # Since strats already preserves the order of bitstruct fields,
+    # we can directly asterisk the generatort to pass in as *args
+    return T( * (draw(strat) for strat in strats) )
+
+  # RETURN A STRATEGY INSTEAD OF FUNCTION
+  return strategy_bitstruct()
--- pymtl3/datatypes/test/strategies_test.py
+++ pymtl3/datatypes/test/strategies_test.py
@@ -13,10 +13,11 @@ import pytest
 
 from pymtl3.datatypes import strategies as pst
 from pymtl3.datatypes.bits_import import *
+from pymtl3.datatypes.bitstructs import bitstruct
 
 
 @pytest.mark.parametrize( 'nbits', [1, 3, 4, 8, 16, 32] )
-def test_unsigned( nbits ):
+def test_bits_unsigned( nbits ):
   print("")
   @hypothesis.given(
     bits = pst.bits(nbits)
@@ -25,10 +26,12 @@ def test_unsigned( nbits ):
   def actual_test( bits ):
     assert bits.nbits == nbits
     print( bits, bits.uint() )
+
+  print("-"*10,nbits,"-"*10)
   actual_test()
 
 @pytest.mark.parametrize( 'nbits', [1, 3, 4, 8, 16, 32] )
-def test_signed( nbits ):
+def test_bits_signed( nbits ):
   print("")
   @hypothesis.given(
     bits = pst.bits(nbits, True)
@@ -37,4 +40,66 @@ def test_signed( nbits ):
   def actual_test( bits ):
     assert bits.nbits == nbits
     print( bits, bits.int() )
+
+  print("-"*10,nbits,"-"*10)
+  actual_test()
+
+def test_bits16_limited():
+  print("")
+  @hypothesis.given(
+    bits = pst.bits(16, min_value=2, max_value=11)
+  )
+  @hypothesis.settings( max_examples=16 )
+  def actual_test( bits ):
+    assert bits.nbits == 16
+    assert 2 <= bits.uint() <= 11
+    print( bits, bits.uint() )
+
+  print("-"*10,16,'limited',"-"*10)
+  actual_test()
+@bitstruct
+class Point1D:
+  x: Bits12
+
+@bitstruct
+class Point2D:
+  x: Bits16
+  y: Bits20
+
+@bitstruct
+class Point3D:
+  x: Bits100
+  y: Bits4
+  z: Bits8
+
+@bitstruct
+class Point3Dx2:
+  x: [ Bits100, Bits100 ]
+  y: [ Bits4, Bits4 ]
+  z: [ Bits8, Bits8 ]
+
+@bitstruct
+class NestedPoint:
+  p1: Point1D
+  p2: Point2D
+  p3: Point3D
+
+@bitstruct
+class NNestedPoint:
+  p1: Point1D
+  p2: Point2D
+  p3: [ Point3Dx2, Point3Dx2 ]
+
+@pytest.mark.parametrize( 'T', [Point1D, Point2D, Point3D, Point3Dx2, NestedPoint , NNestedPoint] )
+def test_bitstruct( T ):
+  print("")
+  @hypothesis.given(
+    bs = pst.bitstruct(T)
+  )
+  @hypothesis.settings( max_examples=16 )
+  def actual_test( bs ):
+    assert isinstance( bs, T )
+    print( bs )
+
+  print("-"*10,T,"-"*10)
   actual_test()
