5590f1110fd92ddac7734562e4a6ad256e190d41
Shunning Jiang <sj634@cornell.edu>
2019-12-10 15:46:10 -0500

    document the problem

--- pymtl3/passes/sim/SimpleTickPass.py
+++ pymtl3/passes/sim/SimpleTickPass.py
@@ -12,6 +12,12 @@ from pymtl3.dsl.errors import UpblkCyclicError
 from pymtl3.passes.BasePass import BasePass
 from pymtl3.passes.errors import PassOrderError
 
+# Shunning: We are aware of the problem that there may be multiple places
+# that assembles the function for the user to tick the simulator.
+# I think LLVM applies passes based on the order they are inserted to the
+# pass manager. LLVM also has some local ordering mechanism to figure out
+# the dependencies between passes. Currently we haven't got to the point
+# where we have enough passes to manage, but we should keep this in mind.
 
 class SimpleTickPass( BasePass ):
 
