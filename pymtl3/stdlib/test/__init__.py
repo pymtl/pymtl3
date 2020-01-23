a161f1269105bbf007780902af187d53ea69a809
Shunning Jiang <sj634@cornell.edu>
2020-01-22 21:47:38 -0500

    fix isort

--- pymtl3/stdlib/test/__init__.py
+++ pymtl3/stdlib/test/__init__.py
@@ -1,3 +1,8 @@
 from .test_sinks import TestSinkCL
 from .test_srcs import TestSrcCL
-from .test_utils import TestVectorSimulator, run_sim, run_test_vector_sim, mk_test_case_table
+from .test_utils import (
+    TestVectorSimulator,
+    mk_test_case_table,
+    run_sim,
+    run_test_vector_sim,
+)
