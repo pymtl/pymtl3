03094ff8c2f31f3921a420fb3a9012fcd433cc10
Shunning Jiang <sj634@cornell.edu>
2020-01-22 21:27:29 -0500

    implement run_test_vector_sim

--- pymtl3/stdlib/test/__init__.py
+++ pymtl3/stdlib/test/__init__.py
@@ -1,3 +1,3 @@
 from .test_sinks import TestSinkCL
 from .test_srcs import TestSrcCL
-from .test_utils import TestVectorSimulator, mk_test_case_table
+from .test_utils import TestVectorSimulator, run_sim, run_test_vector_sim, mk_test_case_table
--- pymtl3/stdlib/test/test_utils.py
+++ pymtl3/stdlib/test/test_utils.py
@@ -1,6 +1,7 @@
 import collections
 
 from pymtl3 import *
+from pymtl3.datatypes import is_bitstruct_class
 
 #-------------------------------------------------------------------------
 # mk_test_case_table
@@ -67,3 +68,181 @@ class TestVectorSimulator:
       self.verify_outputs_func( self.model, test_vector )
 
       self.model.tick()
+
+def run_sim( model, dump_vcd=None, test_verilog=False, max_cycles=5000 ):
+
+  # Setup the model
+
+  if dump_vcd:
+    model.config_tracing = TracingConfigs( tracing='vcd', vcd_file_name=dump_vcd )
+
+  if test_verilog:
+    model = TranslationTool( model )
+
+  # Create a simulator
+
+  model.apply( SimulationPass() )
+
+  # Reset model
+
+  model.sim_reset()
+  print()
+
+  # Run simulation
+
+  ncycles = 0
+  while not model.done() and ncycles < max_cycles:
+    print( f"{ncycles:3}: {model.line_trace()}" )
+    model.tick()
+    ncycles += 1
+
+  # Force a test failure if we timed out
+
+  assert ncycles < max_cycles
+
+  # Extra ticks to make VCD easier to read
+
+  model.tick()
+  model.tick()
+  model.tick()
+
+
+def run_test_vector_sim( model, test_vectors, dump_vcd=None, test_verilog=False ):
+
+  # First row in test vectors contains port names
+
+  if isinstance(test_vectors[0],str):
+    port_names = test_vectors[0].split()
+  else:
+    port_names = test_vectors[0]
+
+  # Remaining rows contain the actual test vectors
+
+  test_vectors = test_vectors[1:]
+
+  # Setup the model
+
+  if dump_vcd:
+    model.config_tracing = TracingConfigs( tracing='vcd', vcd_file_name=dump_vcd )
+  if test_verilog:
+    model = TranslationTool( model )
+
+  # Create a simulator
+
+  model.apply( SimulationPass() )
+
+  # Reset model
+
+  model.sim_reset()
+  print("")
+
+  # Run the simulation
+
+  ncycles = 0
+  row_num = 0
+
+  in_ids  = []
+  out_ids = []
+
+  groups = [ None ] * len(port_names)
+  types  = [ None ] * len(port_names)
+
+  # Preprocess default type
+  # Special case for lists of ports
+  # NOTE THAT WE ONLY SUPPORT 1D ARRAY and no interface
+
+  for i, port_full_name in enumerate( port_names ):
+    if port_full_name[-1] == "*":
+      out_ids.append( i )
+      port_name = port_full_name[:-1]
+    else:
+      in_ids.append( i )
+      port_name = port_full_name
+
+    if '[' in port_name:
+
+      # Get tokens of the full name
+
+      m = re.match( r'(\w+)\[(\d+)\]', port_name )
+
+      if not m:
+        raise Exception(f"Could not parse port name: {port_name}. "
+                        f"Currently we don't support interface or high-D array.")
+
+      groups[i] = g = ( True, m.group(1), m.group(2) )
+
+      # Get type of all the ports
+
+      t = type( getattr( model, g[1] )[ g[2] ] )
+      types[i] = None if is_bitstruct_class( t ) else t
+
+    else:
+      groups[i] = ( False, port_name )
+      t = type( getattr( model, port_name ) )
+      types[i] = None if is_bitstruct_class( t ) else t
+
+  for row in test_vectors:
+    row_num += 1
+
+    # Apply test inputs
+
+    for i in in_ids:
+
+      in_value = row[i]
+      t = types[i]
+      if t:
+        in_value = t( in_value )
+
+      g = groups[i]
+      if g[0]:
+        getattr( model, g[1] )[g[2]] = in_value
+      else:
+        setattr( model, g[1], in_value )
+
+    # Evaluate combinational concurrent blocks
+
+    model.eval_combinational()
+
+    # Display line trace output
+
+    print(f"{ncycles}: {model.line_trace()}")
+
+    # Check test outputs
+
+    for i in out_ids:
+      ref_value = row[i]
+      if ref_value == '?':
+        continue
+
+      g = groups[i]
+      if g[0]:
+        out_value = getattr( model, g[1] )[g[2]]
+      else:
+        out_value = getattr( model, g[1] )
+
+      if out_value != ref_value:
+
+        error_msg = """
+run_test_vector_sim received an incorrect value!
+- row number     : {row_number}
+- port name      : {port_name}
+- expected value : {expected_msg}
+- actual value   : {actual_msg}
+"""
+        raise RunTestVectorSimError( error_msg.format(
+          row_number   = row_num,
+          port_name    = port_name,
+          expected_msg = ref_value,
+          actual_msg   = out_value
+        ))
+
+    # Tick the simulation
+
+    model.tick()
+    ncycles += 1
+
+  # Extra ticks to make VCD easier to read
+
+  model.tick()
+  model.tick()
+  model.tick()
