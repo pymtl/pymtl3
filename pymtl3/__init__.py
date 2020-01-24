246ca92520f551a885810120878a194664c119e3
Shunning Jiang <sj634@cornell.edu>
2020-01-24 14:20:27 -0500

    import tracing configs and sverilog passes by default

--- pymtl3/__init__.py
+++ pymtl3/__init__.py
@@ -2,6 +2,8 @@ from .datatypes import *
 from .datatypes import _bitwidths
 from .dsl import *
 from .passes.PassGroups import SimulationPass
+from .passes.backends.sverilog import TranslationImportPass, ImportPass
+from .passes import TracingConfigs, ImportConfigs
 
 __version__ = "0.5.4"
 
@@ -11,7 +13,7 @@ __all__ = [
   'connect', 'method_port',
   'non_blocking', 'NonBlockingCalleeIfc', 'NonBlockingCallerIfc', 'blocking',
 
-  'SimulationPass',
+  'SimulationPass', 'TracingConfigs', 'TranslationImportPass', 'ImportPass',
   'Component', 'Placeholder',
 
   'sext', 'zext', 'clog2', 'concat', 'reduce_and', 'reduce_or', 'reduce_xor',
