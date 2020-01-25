996e395777207711cb9a26f58cc380d4f4210122
Shunning Jiang <sj634@cornell.edu>
2020-01-24 20:58:12 -0500

    fix leftover variables

--- pymtl3/__init__.py
+++ pymtl3/__init__.py
@@ -1,6 +1,26 @@
 from .datatypes import *
 from .datatypes import _bitwidths
-from .dsl import *
+from .dsl.Component import Component
+from .dsl.ComponentLevel3 import connect
+from .dsl.ComponentLevel5 import method_port
+from .dsl.ComponentLevel6 import non_blocking
+from .dsl.ComponentLevel7 import blocking
+from .dsl.Connectable import (
+    CalleeIfcCL,
+    CalleeIfcFL,
+    CalleeIfcRTL,
+    CalleePort,
+    CallerIfcCL,
+    CallerIfcFL,
+    CallerIfcRTL,
+    CallerPort,
+    InPort,
+    Interface,
+    OutPort,
+    Wire,
+)
+from .dsl.ConstraintTypes import RD, WR, M, U
+from .dsl.Placeholder import Placeholder
 from .passes import ImportConfigs, TracingConfigs
 from .passes.backends.sverilog import ImportPass, TranslationImportPass
 from .passes.PassGroups import SimulationPass
@@ -11,7 +31,9 @@ __all__ = [
   'U','M','RD','WR',
   'Wire', 'InPort', 'OutPort', 'Interface', 'CallerPort', 'CalleePort',
   'connect', 'method_port',
-  'non_blocking', 'NonBlockingCalleeIfc', 'NonBlockingCallerIfc', 'blocking',
+  'CalleeIfcRTL', 'CallerIfcRTL',
+  'non_blocking', 'CalleeIfcCL', 'CallerIfcCL',
+  'blocking', 'CalleeIfcFL', 'CallerIfcFL',
 
   'SimulationPass', 'TracingConfigs', 'TranslationImportPass', 'ImportPass',
   'Component', 'Placeholder',
