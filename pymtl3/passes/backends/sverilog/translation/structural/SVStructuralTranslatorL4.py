8a8f753f2f3b4c66d11ed566e00ec727556932b4
pp482 <pp482@cornell.edu>
2020-02-04 18:38:05 -0500

    Add support for non-top nested interfaces

--- pymtl3/passes/backends/sverilog/translation/structural/SVStructuralTranslatorL4.py
+++ pymtl3/passes/backends/sverilog/translation/structural/SVStructuralTranslatorL4.py
@@ -42,8 +42,12 @@ class SVStructuralTranslatorL4(
     }
 
   def rtlir_tr_subcomp_ifc_port_decls( s, _ifc_port_decls ):
-    port_defs = [x['def'] for x in _ifc_port_decls]
-    port_decls = [x['decl'] for x in _ifc_port_decls]
+    port_defs, port_decls = [], []
+
+    for ifc_port_decls in _ifc_port_decls:
+      port_defs  += [x['def'] for x in ifc_port_decls]
+      port_decls += [x['decl'] for x in ifc_port_decls]
+
     make_indent( port_defs, 1 )
     make_indent( port_decls, 2 )
     return {
@@ -54,19 +58,49 @@ class SVStructuralTranslatorL4(
   def rtlir_tr_subcomp_ifc_port_decl( s, m, c_id, c_rtype, c_array_type,
       ifc_id, ifc_rtype, ifc_array_type, port_id, port_rtype,
       port_array_type ):
-    assert isinstance( port_rtype, rt.Port ), \
-      f"SystemVerilog backend does not support nested interface {port_id} yet!"
-    port_dtype = s.rtlir_data_type_translation( m, port_rtype.get_dtype() )
-    port_def_rtype = rt.Wire(port_dtype["raw_dtype"])
 
-    return {
-      'def' : s.rtlir_tr_wire_decl('{c_id}__{ifc_id}__'+port_id, port_def_rtype,
-                port_array_type, port_dtype),
-      'decl' : f'.{{ifc_id}}__{port_id}( {{c_id}}__{{ifc_id}}__{port_id} )'
-    }
+    def _flatten_ifc_array( cur_ifc_id, n_dim ):
+      nonlocal s, m, c_id, c_rtype, c_array_type
+      nonlocal port_id, port_rtype, port_array_type
+      ret = []
+
+      if not n_dim:
+        all_ifc_properties = port_rtype.get_all_properties_packed()
+        for _port_id, _port_rtype in all_ifc_properties:
+          if isinstance( _port_rtype, rt.Array ):
+            _port_array_rtype = _port_rtype
+            _port_rtype = _port_rtype.get_sub_type()
+          else:
+            _port_array_rtype = None
+            _port_rtype = _port_rtype
+
+          ret += s.rtlir_tr_subcomp_ifc_port_decl( m, c_id, c_rtype, c_array_type,
+                   f"{cur_ifc_id}__{port_id}", port_rtype, port_array_type,
+                   _port_id, _port_rtype, s.rtlir_tr_unpacked_array_type( _port_array_rtype ) )
+
+      else:
+        for i in range( n_dim[0] ):
+          ret += _flatten_ifc_array( f"{cur_ifc_id}__{i}", n_dim[1:] )
+
+      return ret
+
+    if isinstance( port_rtype, rt.Port ):
+      port_dtype = s.rtlir_data_type_translation( m, port_rtype.get_dtype() )
+      port_def_rtype = rt.Wire(port_dtype["raw_dtype"])
+
+      return [ {
+        'def' : s.rtlir_tr_wire_decl(f'{{c_id}}__{ifc_id}__{port_id}', port_def_rtype,
+                  port_array_type, port_dtype),
+        'decl' : f'.{ifc_id}__{port_id}( {{c_id}}__{ifc_id}__{port_id} )'
+      } ]
+
+    else:
+      # Support nested interface
+      assert isinstance( port_rtype, rt.InterfaceView )
+      return _flatten_ifc_array( ifc_id, port_array_type['n_dim'] )
 
   def rtlir_tr_subcomp_ifc_decls( s, _ifc_decls ):
-    _ifc_decls = sum( _ifc_decls, [] )
+    # _ifc_decls = sum( _ifc_decls, [] )
     ifc_defs = [x['def'] for x in _ifc_decls]
     ifc_decls = [x['decl'] for x in _ifc_decls]
     return {
@@ -76,22 +110,7 @@ class SVStructuralTranslatorL4(
 
   def rtlir_tr_subcomp_ifc_decl( s, m, c_id, c_rtype, c_array_type,
       ifc_id, ifc_rtype, ifc_array_type, ports ):
-
-    def gen_subcomp_ifc_decl( ifc_id, ifc_rtype, n_dim, c_n_dim, ports ):
-      if not n_dim:
-        c_id = '{c_id}'
-        ifc_id = ifc_id + c_n_dim
-        return [ {
-          'def' : ports['def'].format( **locals() ),
-          'decl' : ports['decl'].format( **locals() )
-        } ]
-      else:
-        return sum( [gen_subcomp_ifc_decl( ifc_id, ifc_rtype, n_dim[1:],
-            c_n_dim+'__'+str( idx ), ports ) for idx in range( n_dim[0] )], [] )
-
-    n_dim = ifc_array_type[ 'n_dim' ]
-    return \
-      gen_subcomp_ifc_decl( ifc_id, ifc_rtype, n_dim, "", ports )
+    return ports
 
   def rtlir_tr_subcomp_decls( s, subcomps ):
     subcomp_decls = sum( subcomps, [] )
