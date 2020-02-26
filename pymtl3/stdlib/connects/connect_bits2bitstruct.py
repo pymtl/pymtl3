5324433638abfca3f75de301e8a80461e62996ec
yo96 <yo96@cornell.edu>
2020-02-26 13:19:02 -0500

    helper function returns the stop index

--- pymtl3/stdlib/connects/connect_bits2bitstruct.py
+++ pymtl3/stdlib/connects/connect_bits2bitstruct.py
@@ -16,7 +16,7 @@ from pymtl3.datatypes.bitstructs import _FIELDS, is_bitstruct_class
 #-------------------------------------------------------------------------
 # Helper function for connect_bits2bitstruct.
 
-def _connect_bits2bitstruct_h( field, bits_signal, slices ):
+def _connect_bits2bitstruct_h( field, bits_signal, start ):
 
   # NOTE: We do not support list of fields at the moment because: 1) there
   # are very few use cases; 2) the pack order of a list of fields is
@@ -27,18 +27,18 @@ def _connect_bits2bitstruct_h( field, bits_signal, slices ):
   field_type = field.get_type()
 
   if issubclass( field_type, Bits ):
-    start     = 0 if not slices else slices[-1].stop
     stop      = start + field_type.nbits
     cur_slice = slice( start, stop )
-    slices.append( cur_slice )
     connect( field, bits_signal[ cur_slice ] )
+    return stop
 
   else:
-    assert is_bitstruct_class( field_type )
     fields_dict = getattr( field_type, _FIELDS )
+    cur_stop    = start
     for fname, _ in reversed( list( fields_dict.items() ) ):
       subfield = getattr( field, fname )
-      _connect_bits2bitstruct_h( subfield, bits_signal, slices )
+      cur_stop = _connect_bits2bitstruct_h( subfield, bits_signal, cur_stop )
+    return cur_stop
 
 #-------------------------------------------------------------------------
 # connect_bits2bitstruct
@@ -72,4 +72,4 @@ def connect_bits2bitstruct( signal1, signal2 ):
     f'but {type2.__qualname__} is {bw2}-bit.'
 
   # Connect field to corresponding slice
-  _connect_bits2bitstruct_h( bitstruct_signal, bits_signal, [] )
+  _connect_bits2bitstruct_h( bitstruct_signal, bits_signal, 0 )
