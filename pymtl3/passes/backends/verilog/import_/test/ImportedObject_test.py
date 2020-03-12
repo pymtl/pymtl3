fd6bdccd974d9d8c8819e2a3cb48777a087608b3
pp482 <pp482@cornell.edu>
2020-03-11 21:27:54 -0400

    Fix a bug in importing parametrized Verilog

--- pymtl3/passes/backends/verilog/import_/test/ImportedObject_test.py
+++ pymtl3/passes/backends/verilog/import_/test/ImportedObject_test.py
@@ -251,6 +251,54 @@ def test_normal_queue( do_test ):
   q._tv_out = tv_out
   do_test( q )
 
+def test_normal_queue_implicit_top_module( do_test ):
+  # Test a Placeholder with params in `construct`
+  def tv_in( m, tv ):
+    m.enq_en = Bits1( tv[0] )
+    m.enq_msg = Bits32( tv[1] )
+    m.deq_en = Bits1( tv[3] )
+  def tv_out( m, tv ):
+    if tv[2] != '*':
+      assert m.enq_rdy == Bits1( tv[2] )
+    if tv[4] != '*':
+      assert m.deq_rdy == Bits1( tv[5] )
+    if tv[5] != '*':
+      assert m.deq_msg == Bits32( tv[4] )
+  class VQueue( Component, Placeholder ):
+    def construct( s, data_width, num_entries, count_width ):
+      s.count   =  OutPort( mk_bits( count_width )  )
+      s.deq_en  =  InPort( Bits1  )
+      s.deq_rdy = OutPort( Bits1  )
+      s.deq_msg = OutPort( mk_bits( data_width ) )
+      s.enq_en  =  InPort( Bits1  )
+      s.enq_rdy = OutPort( Bits1  )
+      s.enq_msg =  InPort( mk_bits( data_width ) )
+      s.config_placeholder = VerilogPlaceholderConfigs(
+          src_file = dirname(__file__)+'/VQueue.v',
+      )
+      s.verilog_translate_import = True
+  num_entries = 1
+  q = VQueue(
+      data_width = 32,
+      num_entries = num_entries,
+      count_width = clog2(num_entries+1))
+  # q.dump_vcd = True
+  test_vector = [
+    #   enq                deq
+    #   en    msg   rdy    en    msg   rdy
+    [    1,    42,    1,    0,     0,    0  ],
+    [    0,    43,    0,    1,    42,    1  ],
+    [    1,    43,    1,    0,    42,    0  ],
+    [    0,    44,    0,    1,    43,    1  ],
+    [    1,    44,    1,    0,    43,    0  ],
+    [    0,    45,    0,    1,    44,    1  ],
+    [    1,    45,    1,    0,    44,    0  ],
+  ]
+  q._test_vectors = test_vector
+  q._tv_in = tv_in
+  q._tv_out = tv_out
+  do_test( q )
+
 def test_normal_queue_params( do_test ):
   # Test the `params` option of placeholder configs
   def tv_in( m, tv ):
