14d43fbf09265a76603f763732249ca8f1b67dbc
pp482 <pp482@cornell.edu>
2020-03-19 17:51:13 -0400

    Remove redundant debug print

--- pymtl3/passes/backends/verilog/__init__.py
+++ pymtl3/passes/backends/verilog/__init__.py
@@ -2,5 +2,3 @@ from .import_.VerilatorImportPass import VerilatorImportPass
 from .translation.TranslationPass import TranslationPass
 from .TranslationImportPass import TranslationImportPass
 from .VerilogPlaceholderPass import VerilogPlaceholderPass
-
-print(f"VerilogPlaceholderPass = {VerilogPlaceholderPass}")
