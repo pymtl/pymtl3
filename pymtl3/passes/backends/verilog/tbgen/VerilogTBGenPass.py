45708557a0655de2cf7781e26f0c136c0fbb2461
Shunning Jiang <sj634@cornell.edu>
2020-03-21 13:34:23 -0400

    working?!

--- pymtl3/passes/backends/verilog/tbgen/VerilogTBGenPass.py
+++ pymtl3/passes/backends/verilog/tbgen/VerilogTBGenPass.py
@@ -4,26 +4,16 @@
 # Author : Shunning Jiang
 # Date   : Mar 18, 2020
 
-import copy
-import importlib
-import json
-import linecache
 import os
-import shutil
-import subprocess
+import py
 import sys
-from importlib import reload
-from itertools import cycle
-from textwrap import indent
 from collections import deque
 
-from pymtl3 import Placeholder
-from pymtl3.datatypes import get_nbits
-from pymtl3.dsl import Component
-from pymtl3.passes.BasePass import BasePass
+from pymtl3.datatypes import to_bits, get_nbits
+from pymtl3.passes.BasePass import BasePass, PassMetadata
 from pymtl3.passes.rtlir import RTLIRDataType as rdt
 from pymtl3.passes.rtlir import RTLIRType as rt
-from pymtl3.passes.rtlir import get_component_ifc_rtlir
+from pymtl3.utils import custom_exec
 
 from ..errors import VerilogImportError
 from ..util.utility import (
@@ -43,6 +33,9 @@ class VerilogTBGenPass( BasePass ):
       raise VerilogImportError( top,
         f"please elaborate design {top} before applying the import pass!" )
 
+    top._tbgen = PassMetadata()
+    top._tbgen.tbgen_hooks = []
+
     tbgen_components = []
 
     def traverse_hierarchy( m ):
@@ -57,27 +50,27 @@ class VerilogTBGenPass( BasePass ):
     for x in tbgen_components:
 
       signal_decls = []
-
-      dut_signal_decls = []
-
       task_signal_decls = []
       task_assign_strs = []
       task_check_strs = []
+      dut_signal_decls = []
 
-      pname_vname_mapping = {}
+      py_signal_order = []
 
       for pname, vname, port, is_ifc in x._ports:
         if vname == "reset" or vname == "clk":
           continue
-        print(self._get_direction(port), pname, vname, port, is_ifc)
 
         # Prepare for generating strings
+        if   isinstance( port, rt.Port ):  direction = port.get_direction()
+        elif isinstance( port, rt.Array ): direction = port.get_sub_type().get_direction()
+        else:                              raise Exception( f"unrecognized direction {d}!" )
 
-        direction = self._get_direction( port )
         p_n_dim, p_rtype = get_rtype( port )
         dtype = p_rtype.get_dtype()
         if   isinstance( dtype, rdt.Vector ): nbits = dtype.get_length()
         elif isinstance( dtype, rdt.Struct ): nbits = get_nbits(dtype.get_class())
+        else:                                 raise Exception( f"unrecognized data type {d}!" )
 
         # signal_decls
         signal_decl_indices = " ".join( [ f"[0:{d-1}]" for d in p_n_dim ] )
@@ -86,29 +79,28 @@ class VerilogTBGenPass( BasePass ):
         # dut_signal_decls
         dut_signal_decls.append( f".{vname}({vname})" )
 
-        # task related strs
-        prefix = "inp_" if direction == "input" else "ref_"
-
         Q = deque( [ (vname, vname, p_n_dim) ] )
+        tot = 0
         while Q:
           name, mangled_name, indices = Q.popleft()
           if not indices:
+            pyname = pname[tot]
             if direction == "input":
               task_signal_decls.append( f"input logic [{nbits-1}:0] inp_{mangled_name}" )
               task_assign_strs.append( f"{name} = inp_{mangled_name}")
             else: # output
               task_signal_decls.append( f"input logic [{nbits-1}:0] ref_{mangled_name}" )
-              task_check_strs.append( f"if ({name} != ref_{mangled_name}) $finish")
+              task_check_strs.append( f"`CHECK(lineno, {name}, ref_{mangled_name}, \"{pyname} ({name} in Verilog)\")")
+            tot += 1
+            py_signal_order.append(pyname)
           else:
             for i in range( indices[0] ):
               Q.append( (f"{name}[{i}]", f"{mangled_name}__{i}", indices[1:]) )
 
-      print()
-
       dut_name = x._ip_cfg.translated_top_module
 
       strs = {
-        'args_strs': ",".join([f"a{i}" for i in range(len(x._ports))]),
+        'args_strs': ",".join([f"a{i}" for i in range(len(task_signal_decls))]),
         'harness_name': dut_name + "_tb",
         'signal_decls': ";\n  ".join(signal_decls), # logic [31:0] xxx, -- packed array
         'task_signal_decls': ",\n    ".join(task_signal_decls), # input logic [31:0] in__x;input logic [31:0] ref_y; -- unpacked ports
@@ -118,13 +110,8 @@ class VerilogTBGenPass( BasePass ):
         'dut_clk_decl': '.clk(clk)' if x._ph_cfg.has_clk else '',
         'dut_reset_decl': '.reset(reset)' if x._ph_cfg.has_reset else '',
         'dut_signal_decls': ",\n    ".join(dut_signal_decls), # logic [31:0] xxx, -- packed array, # .x(x), -- packed array
-        'test_cases_file_name': '',
+        'test_cases_file_name': dut_name+"_tb.v.cases",
       }
-
-      print()
-      for x,y in strs.items():
-        print(repr(x),y)
-
       template_name = os.path.dirname( os.path.abspath( __file__ ) ) + \
                       os.path.sep + 'verilog_tbgen.v.template'
 
@@ -133,35 +120,18 @@ class VerilogTBGenPass( BasePass ):
           template = template.read()
           output.write( template.format( **strs ) )
 
-  def collect_sig_func( self, top ):
+      case_file = open( dut_name + "_tb.v.cases", "w" )
+      top._tbgen.tbgen_hooks.append( self.gen_hook_func( top, py_signal_order, case_file ) )
 
-    # TODO use actual nets to reduce the amount of saved signals
+  @staticmethod
+  def gen_hook_func( top, ports, case_file ):
+    port_srcs = [ f"{{hex(int(to_bits(top.{p})))}}" for p in ports ]
 
-    # Give all ' and " characters a preceding backslash for .format
-    wav_srcs = []
-
-    # Now we create per-cycle signal value collect functions
-    for x in top._dsl.all_signals:
-      if x.is_top_level_signal() and ( not repr(x).endswith('.clk') or x is top.clk ):
-        if is_bitstruct_class( x._dsl.Type ):
-          wav_srcs.append( "wavmeta.text_sigs['{0}'].append( to_bits({0}).bin() )".format(x) )
-        elif issubclass( x._dsl.Type, Bits ):
-          wav_srcs.append( "wavmeta.text_sigs['{0}'].append( {0}.bin() )".format(x) )
-
-    wavmeta.text_sigs = defaultdict(list)
-
-    # TODO use integer index instead of dict, should be easy
     src =  """
-def dump_wav():
-  {}
-""".format( "\n  ".join(wav_srcs) )
-    s, l_dict = top, {}
-    exec(compile( src, filename="temp", mode="exec"), globals().update(locals()), l_dict)
-    return l_dict['dump_wav']
+def dump_case():
+  print(f"`T({});", file=case_file)
+""".format( ",".join(port_srcs) )
+    _locals = {}
+    custom_exec( py.code.Source(src).compile(), {'top': top, 'to_bits': to_bits, 'case_file':case_file}, _locals)
+    return _locals['dump_case']
 
-  def _get_direction( s, port ):
-    if isinstance( port, rt.Port ):
-      return port.get_direction()
-    elif isinstance( port, rt.Array ):
-      return port.get_sub_type().get_direction()
-    raise Exception( f"unrecognized direction {d}!" )
