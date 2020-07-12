5672dece195f37ff4fe599d555df9ae4d4e2e439
Shunning Jiang <sj634@cornell.edu>
2020-07-12 01:55:01 -0400

    change the rom impl to multi-ported statically constructed ROM

--- pymtl3/stdlib/mem/ROM.py
+++ pymtl3/stdlib/mem/ROM.py
@@ -2,47 +2,34 @@
 ========================================================================
 ROM.py
 ========================================================================
-LUT-based ROM
+Multiported ROM
 
 Author : Shunning Jiang
 Date   : June 18, 2020
 """
 
 from pymtl3 import *
-from pymtl3.extra.pypy import custom_exec
 
 class ROM( Component ):
 
-  def construct( s, Type, num_entries, data ):
+  def construct( s, Type, num_entries, data, num_ports=1 ):
     assert len(data) == num_entries
 
-    s.addr = InPort( clog2(num_entries) )
-    s.out  = OutPort( Type )
+    s.raddr = [ InPort( clog2(num_entries) ) for _ in range(num_ports) ]
+    s.rdata = [ OutPort( Type )              for _ in range(num_ports) ]
 
-    # @update
-    # def up_read_rom():
-    #   if   s.addr == 0: s.out @= 8
-    #   elif s.addr == 1: s.out @= 7
-    #   elif s.addr == 2: s.out @= 6
-    #   elif s.addr == 3: s.out @= 5
-    #   elif s.addr == 4: s.out @= 4
-    #   elif s.addr == 5: s.out @= 3
-    #   elif s.addr == 6: s.out @= 2
-    #   elif s.addr == 7: s.out @= 1
-    #   else: s.out @= 0
+    s.mem = [ Wire(Type) for _ in range(num_entries) ]
+    for i in range(num_entries):
+      s.mem[i] //= data[i]
 
-    import py
-    strs = '\n      elif '.join( [ f's.addr == {i}: s.out @= {x}' for i, x in enumerate(data) ] )
-    custom_exec( py.code.Source(f'''
     @update
-    def up_rom_read():
-      if {strs}
-      else: s.out @= 0
-    ''').compile(), {**globals(), **locals()} )
+    def up_read_rom():
+      for i in range(num_ports):
+        s.rdata[i] @= s.mem[ s.raddr[i] ]
 
   #-----------------------------------------------------------------------
   # line_trace
   #-----------------------------------------------------------------------
 
   def line_trace( s ):
-    return f"[{s.addr}] >>> {s.out}"
+    return "|".join( [ f"[{s.raddr[i]}]->{s.rdata[i]}" for i in range(len(s.raddr)) ] )
--- pymtl3/stdlib/mem/test/ROM_test.py
+++ pymtl3/stdlib/mem/test/ROM_test.py
@@ -7,9 +7,17 @@ from pymtl3.stdlib.test_utils import run_test_vector_sim
 
 from ..ROM import ROM
 
-def test_rom( cmdline_opts ):
-  run_test_vector_sim( ROM(Bits32, 8, [8,7,6,5,4,3,2,1]), [
-    ('addr', 'out*'),
-    [ 1,      7 ],
-    [ 2,      6 ],
-  ], cmdline_opts )
+def test_rom():
+  run_test_vector_sim( ROM(Bits32, 8, [8,7,6,5,4,3,2,1], num_ports=2), [
+    ('raddr[0]', 'rdata[0]*', 'raddr[1]', 'rdata[1]*'),
+    [ 1,          7,           5,          3          ],
+    [ 2,          6,           7,          1          ],
+  ])
+
+  run_test_vector_sim( ROM(Bits32, 8, [8,7,6,5,4,3,2,1], num_ports=2), [
+    ('raddr[0]', 'rdata[0]*', 'raddr[1]', 'rdata[1]*'),
+    [ 1,          7,           5,          3          ],
+    [ 2,          6,           7,          1          ],
+  ], {'dump_vcd': 'test_rom', 'test_verilog': 'ones', 'dump_vtb': ''} )
+
+
