5ff3c87f5879ae397023ca893b1772d62376e1b3
Shunning Jiang <sj634@cornell.edu>
2020-07-24 08:19:13 -0400

    implement comb and seq roms

--- pymtl3/stdlib/mem/ROMRTL.py
+++ pymtl3/stdlib/mem/ROMRTL.py
@@ -10,7 +10,7 @@ Date   : June 18, 2020
 
 from pymtl3 import *
 
-class ROMRTL( Component ):
+class CombinationalROMRTL( Component ):
 
   def construct( s, Type, num_entries, data, num_ports=1 ):
     assert len(data) == num_entries
@@ -27,6 +27,22 @@ class ROMRTL( Component ):
       for i in range(num_ports):
         s.rdata[i] @= s.mem[ s.raddr[i] ]
 
+class SequentialROMRTL( Component ):
+
+  def construct( s, Type, num_entries, data, num_ports=1 ):
+    assert len(data) == num_entries
+
+    s.raddr = [ InPort( clog2(num_entries) ) for _ in range(num_ports) ]
+    s.rdata = [ OutPort( Type )              for _ in range(num_ports) ]
+
+    s.mem = [ Wire(Type) for _ in range(num_entries) ]
+    for i in range(num_entries):
+      s.mem[i] //= data[i]
+
+    @update_ff
+    def up_read_rom():
+      for i in range(num_ports):
+        s.rdata[i] <<= s.mem[ s.raddr[i] ]
   #-----------------------------------------------------------------------
   # line_trace
   #-----------------------------------------------------------------------
--- pymtl3/stdlib/mem/test/ROMRTL_test.py
+++ pymtl3/stdlib/mem/test/ROMRTL_test.py
@@ -5,19 +5,35 @@
 from pymtl3 import *
 from pymtl3.stdlib.test_utils import run_test_vector_sim
 
-from ..ROMRTL import ROMRTL
+from ..ROMRTL import CombinationalROMRTL, SequentialROMRTL
 
-def test_rom_rtl():
-  run_test_vector_sim( ROMRTL(Bits32, 8, [8,7,6,5,4,3,2,1], num_ports=2), [
+def test_combinational_rom_rtl():
+  run_test_vector_sim( CombinationalROMRTL(Bits32, 8, [8,7,6,5,4,3,2,1], num_ports=2), [
     ('raddr[0]', 'rdata[0]*', 'raddr[1]', 'rdata[1]*'),
     [ 1,          7,           5,          3          ],
     [ 2,          6,           7,          1          ],
   ])
 
-  run_test_vector_sim( ROMRTL(Bits32, 8, [8,7,6,5,4,3,2,1], num_ports=2), [
+  run_test_vector_sim( CombinationalROMRTL(Bits32, 8, [8,7,6,5,4,3,2,1], num_ports=2), [
     ('raddr[0]', 'rdata[0]*', 'raddr[1]', 'rdata[1]*'),
     [ 1,          7,           5,          3          ],
     [ 2,          6,           7,          1          ],
+  ], {'dump_vcd': 'test_rom', 'test_verilog': 'ones', 'dump_vtb': ''} )
+
+
+def test_sequential_rom_rtl():
+  run_test_vector_sim( SequentialROMRTL(Bits32, 8, [8,7,6,5,4,3,2,1], num_ports=2), [
+    ('raddr[0]', 'rdata[0]*', 'raddr[1]', 'rdata[1]*'),
+    [ 1,          '?',         5,          '?'        ],
+    [ 2,          7,           7,          3          ],
+    [ 0,          6,           0,          1          ],
+  ])
+
+  run_test_vector_sim( SequentialROMRTL(Bits32, 8, [8,7,6,5,4,3,2,1], num_ports=2), [
+    ('raddr[0]', 'rdata[0]*', 'raddr[1]', 'rdata[1]*'),
+    [ 1,          '?',         5,          '?'        ],
+    [ 2,          7,           7,          3          ],
+    [ 0,          6,           0,          1          ],
   ], {'dump_vcd': 'test_rom', 'test_verilog': 'ones', 'dump_vtb': ''} )
 
 
